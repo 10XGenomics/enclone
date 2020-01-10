f05a58b58511492935a14d0dd791141b5cc4fa8d
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2020-01-10 14:48:13 -0800

    Add documentation

--- src/proto_io.rs
+++ src/proto_io.rs
@@ -3,6 +3,21 @@
 //!
 //! Code for reading and writing custom proto files
 //!
+//! Protobuf format by default is not suitable for streaming. We need to write our own logic to
+//! track where a message begins and ends. The documentation recommends using a length delimited
+//! format (https://developers.google.com/protocol-buffers/docs/techniques#streaming).
+//!
+//! ## Message Format
+//! We use the following format to write a single protobuf message:
+//! ```text
+//! +------------+----------------------------+
+//! | Length     |      Message               |
+//! | [4 bytes]  |      [Length Bytes]        |
+//! +------------+----------------------------+
+//! ```
+//! In the above diagram,
+//! - `Length` is an unsigned 32 bit integer stored in **Big endian** order.
+//! - If there are multiple messages, they are stored consecutively following the same format.
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use failure::Error;
@@ -14,7 +29,9 @@ use types::EncloneOutputs;
 
 const BUFFER_CAPACITY: usize = 1_000_000;
 
+/// A helper struct to write a length delimited protobuf encoded message into the inner `writer`.
 pub struct ProtoWriter<W: Write> {
+    // Buffer space for storing the encoded message
     encode_buffer: Vec<u8>,
     writer: W,
 }
@@ -26,19 +43,25 @@ impl<W: Write> ProtoWriter<W> {
             writer,
         }
     }
+    /// Writes a single message in length delimited format. Returns the total number
+    /// of bytes written
     pub fn encode_and_write<M>(&mut self, message: M) -> Result<usize, Error>
     where
         M: Message,
     {
+        // Write the message length as a big endian unsigned 32 bit integer
         let encoded_len = message.encoded_len();
-        message.encode(&mut self.encode_buffer)?;
         self.writer.write_u32::<BigEndian>(encoded_len as u32)?;
+
+        // Encode the message in protobuf format and write it to the underlying writer
+        message.encode(&mut self.encode_buffer)?;
         self.writer.write_all(&self.encode_buffer)?;
         self.encode_buffer.clear();
-        Ok(encoded_len)
+        Ok(encoded_len + 4) // +4 because of the u32
     }
 }
 
+/// A helper struct to read a length delimited protobuf encoded message from the inner `reader`.
 pub struct ProtoReader<R: Read> {
     decode_buffer: Vec<u8>,
     reader: R,
@@ -55,18 +78,22 @@ impl<R: Read> ProtoReader<R> {
     where
         M: Message + Default,
     {
+        // Attempt to take 4 bytes from the buffer
         self.reader
             .by_ref()
             .take(4)
             .read_to_end(&mut self.decode_buffer)?;
+        // If we did not get 4 bytes, return an error
         if self.decode_buffer.len() != 4 {
             return Err(format_err!(
                 "Expected to get 4 bytes for length. Got {} bytes!",
                 self.decode_buffer.len()
             ));
         }
+        // decode the 4 bytes as a big endian u32
         let decoded_len = self.decode_buffer.as_slice().read_u32::<BigEndian>()?;
         self.decode_buffer.clear();
+        // Decode the message
         self.reader
             .by_ref()
             .take(decoded_len as u64)
@@ -77,6 +104,24 @@ impl<R: Read> ProtoReader<R> {
     }
 }
 
+/// The enclone outputs are stored in the protobuf file as follows:
+/// ```text
+/// +------------+----------------------------+------------+------------------------+
+/// | Length     |      Universal Reference   | Length     |      Donor Reference   |....
+/// | [4 bytes]  |      [Length Bytes]        | [4 bytes]  |      [Length Bytes]    |....
+/// +------------+----------------------------+------------+------------------------+
+///
+/// +------------+-------------------------------+------------+-------------------------------+
+/// | Length     |      Number of clonotypes (N) | Length     |      Clonotype 0              |....
+/// | [4 bytes]  |      [Length Bytes]           | [4 bytes]  |      [Length Bytes]           |....
+/// +------------+-------------------------------+------------+-------------------------------+
+///
+///     +------------+----------------------------------------+
+/// ... | Length     |      Clonotype N-1                     |
+/// ... | [4 bytes]  |      [Length Bytes]                    |
+///     +------------+----------------------------------------+
+/// ```
+/// The newlines are only showed for illustration
 pub fn write_proto(enclone_outputs: EncloneOutputs, path: impl AsRef<Path>) -> Result<(), Error> {
     let writer = BufWriter::new(File::create(path)?);
     let mut proto_writer = ProtoWriter::with_writer(writer);
@@ -93,6 +138,8 @@ pub fn write_proto(enclone_outputs: EncloneOutputs, path: impl AsRef<Path>) -> R
     Ok(())
 }
 
+/// A read that mirrors the write above. It is possible to stream through the
+/// clonotypes instead of loading everything into memory.
 pub fn read_proto(path: impl AsRef<Path>) -> Result<EncloneOutputs, Error> {
     let reader = BufReader::new(File::open(path)?);
     let mut proto_reader = ProtoReader::with_reader(reader);
