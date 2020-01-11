a5b3b99b757855e252465a8affc78bdc874857cd
wyattmcdonnell <18267139+wyattmcdonnell@users.noreply.github.com>
2020-01-10 16:42:50 -0800

    Update allele.rs
    Copyright fix

--- src/allele.rs
+++ src/allele.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 10X Genomics, Inc. All rights reserved.
+// Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
 
 // This file provides functions to find alternate alleles and substitute them into references.
 
