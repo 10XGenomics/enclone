b8c0b8babbce7cdadd41ef7b10d93a245edbb4be
David Jaffe <david.jaffe@10xgenomics.com>
2020-01-19 07:22:42 -0800

    add code comments on testing

--- src/help1.rs
+++ src/help1.rs
@@ -10,15 +10,21 @@ use std::env;
 use string_utils::*;
 use vector_utils::*;
 
-// Notes on a testing issue.  The code for NOPAGER accidentally broke at one point and we
-// don't want that to recur.  Some test cases that could be verified:
-// enclone BCR=...
-// enclone BCR=... NOPAGER
-// enclone help all
-// enclone help all NOPAGER
-// enclone help faq
-// enclone help faq NOPAGER.
-// It's not clear how to automate this.
+// Notes on some testing issues:
+//
+// 1. The code for NOPAGER accidentally broke at one point and we
+//    don't want that to recur.  Some test cases that could be verified:
+//    enclone BCR=...
+//    enclone BCR=... NOPAGER
+//    enclone help all
+//    enclone help all NOPAGER
+//    enclone help faq
+//    enclone help faq NOPAGER.
+//
+// 2. In OSX Catalina, in full screen mode, at one point enclone appeared to have no output,
+//    because the output was going to the alternate screen.
+//
+// It's not clear how to automate testing for these problems.
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
