e22f018e0c87580396c1b57dc4af49ffdb754f8e
David Jaffe <david.jaffe@10xgenomics.com>
2020-03-01 06:14:47 -0800

    separate out tests

--- tests/enclone_test.rs
+++ tests/enclone_test.rs
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
+
 #![allow(unused_imports, dead_code)]
+
+// somewhat misplaced comments:
+//
 // Run enclone on a test case and verify that the output is identical to what
 // was gotten before.  If the output is different, look at it
 // and decide if the change is justified, and if so update the output file.
@@ -46,6 +50,85 @@ fn test_enclone_fail() {
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
+// Define a bunch of tests.
+
+const TESTS : [&str; 32] = [
+    // 1. tests variant base after CDR3, parseable output
+    "BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout \
+     PCOLS=exact_subclonotype_id,ncells,v_name1,v_name2,nchains,var_indices_aa1,barcodes",
+    // 2. tests many donor ref differences, test comp and var and donorn
+    "BCR=123089 CDR3=CARRYFGVVADAFDIW CVARSP=comp,var AMINO=cdr3,var,share,donorn",
+    // 3. tests motif in CDR3, CHAINS, utot, flipped args in CVARS, on tiny dataset
+    "BCR=85333 CDR3=\"CAA.*\" CHAINS=2 CVARS=const,utot",
+    // 4. tests gex and antibody, FULL_SEQC, ulen, udiff, on tiny dataset
+    "BCR=86237 GEX=85679 LVARSP=gex_med,CD19_ab,CD25_ab,IGLV3-1_g,RPS27_g CELLS=3 FULL_SEQC \
+     CVARSP=ulen,udiff",
+    // 5. tests TCR and correct grouping of onesies on AGBT Donor 2 dataset
+    "TCR=101287 MIN_CELLS=100",
+    // 6. tests AMINO=
+    "BCR=86237 CELLS=3 AMINO= CVARS=umed,rmed,cdr3_dna",
+    // 7. tests SHM deletion
+    "BCR=123085 CVARSP=var,clen,cdiff CDR3=CAREPLYYDFWSAYFDYW LVARSP=near,far",
+    // 8. this clonotype included a junk chain before we made a change
+    "TCR=163911 CDR3=CAPSAGDKIIF AMINO=donor",
+    // 9. tests PER_BC
+    "BCR=85333 CDR3=CAKGDRTGYSYGGGIFDYW PER_BC",
+    // 10. tests multiple datasets and also LVARS=ncells,donors,datasets, and share
+    // Note that we have deliberately "faked" two donors.  In reality there is one.
+    "BCR=\"123085;123089\" CDR3=CVKDRVTGTITELDYW LVARS=ncells,donors,datasets AMINO=share NDONOR",
+    // 11. tests META
+    "META=test/inputs/meta_test11 CDR3=CARSFFGDTAMVMFQAFDPW LVARSP=donors,gex_med",
+    // 12. this added because it got better when a noise filter was added, also tests umax
+    "TCR=163914 CDR3=CASSLVQPSTDTQYF CVARSP=umax",
+    // 13. this added because it got better when a noise filter was added; also test FASTA
+    "TCR=163914 CDR3=CAFRGGSYIPTF FASTA=stdout",
+    // 14. this added because it got better when a bug in bads detection was fixed
+    "TCR=163914 CDR3=CASRLGGEETQYF",
+    // 15. tests insertion
+    "BCR=86233 CDR3=CVTEGKGDSVYLEKW CVARS=notes AMINO=cdr3,var",
+    // BCR=123085 CDR3=CARHPAPNYGFWSGYYKTDNWFDPW ==> alt example if we need to dump 86233
+    // 16. tests number of cells broken out by dataset
+    "BCR=123085,123089 LVARS=ncells,n_123085,n_123089 CDR3=CTRDRDLRGATDAFDIW",
+    // 17. tests gex with PER_BC and tests n_gex
+    // See also enclone_test_prebuild below, that tests nearly the same thing,
+    // and tests versus the same output file.
+    "BCR=86237 GEX=85679 LVARSP=gex_max,gex_med,n_gex,CD19_ab CELLS=3 PER_BC",
+    // 18. makes sure cross filtering is isn't applied to two samples from same donor
+    "BCR=123085:123089 CDR3=CVRDEGGARPNKWNYEGAFDIW",
+    // 19. there was a bug that caused twosie to be deleted, and there was foursie junk
+    "BCR=123085 CDR3=CARRYFGVVADAFDIW",
+    // 20. example affected by whitelist (gel bead oligo contamination) filtering
+    "BCR=52177 AMINO=cdr3 PER_BC CDR3=CATWDDSLSGPNWVF",
+    // 21. test MIN_CHAINS_EXACT
+    "BCR=123089 CDR3=CGTWHSNSKPNWVF MIN_CHAINS_EXACT=3",
+    // 22. there was a FP
+    "BCR=\"165807;165808\" FAIL_ONLY=true EXPECT_NULL",
+    // 23. here we were generating a fake alternate allele
+    "BCR=83808 CDR3=CAREGRGMVTTNPFDYW MIN_CELLS_EXACT=30",
+    // 24. an example that uses IGHE
+    "BCR=52177 CDR3=CSTGWGLDFDFWSGYYTAGYHW",
+    // 25. add mouse B6 example that had messed up constant regions
+    "TCR=74396 MOUSE CVARSP=cdiff CDR3=CASSDAGDTQYF",
+    // 26. tests multiple datasets and also LVARS=ncells,donors,datasets, and share
+    // Note that we have deliberately "faked" two donors.  In reality there is one.
+    // Here we make sure that non-specification of NDONOR works.
+    "BCR=\"123085;123089\" CDR3=CVKDRVTGTITELDYW",
+    // 27. tests SUMMARY and NOPRINT
+    "BCR=123085 SUMMARY SUMMARY_CLEAN NOPRINT",
+    // 28. tests BARCODE option
+    "BCR=165807 BARCODE=CCCATACGTGATGATA-1,TCTATTGAGCTGAAAT-1",
+    // 29. tests parenthesized variable in F, SUM and MEAN
+    "BCR=86237 GEX=85679 LVARSP=IGHV3-7_g F=\"(IGHV3-7_g)>=4.5\" MIN_CHAINS=2 SUM MEAN",
+    // 30. tests d_univ and d_donor
+    "BCR=123085 CVARSP=d_univ,d_donor CDR3=CVKDRVTGTITELDYW",
+    // 31. tests Cell Ranger 3.1 output
+    "BCR=../3.1/123085 CDR3=CVKDRVTGTITELDYW",
+    // 32. tests Cell Ranger 2.0 output and RE
+    "BCR=../2.0/124550 CDR3=CAREPLYYDFWSAYFDYW RE",
+];
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
 // The following is a single test, containing many subtests, each of which is a regression test
 // for a given enclone command line.
 
@@ -54,88 +137,14 @@ fn test_enclone_fail() {
 fn test_enclone() {
     PrettyTrace::new().on();
     let t = Instant::now();
-    let tests = vec![
-        // 1. tests variant base after CDR3, parseable output
-        "BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout \
-         PCOLS=exact_subclonotype_id,ncells,v_name1,v_name2,nchains,var_indices_aa1,barcodes",
-        // 2. tests many donor ref differences, test comp and var and donorn
-        "BCR=123089 CDR3=CARRYFGVVADAFDIW CVARSP=comp,var AMINO=cdr3,var,share,donorn",
-        // 3. tests motif in CDR3, CHAINS, utot, flipped args in CVARS, on tiny dataset
-        "BCR=85333 CDR3=\"CAA.*\" CHAINS=2 CVARS=const,utot",
-        // 4. tests gex and antibody, FULL_SEQC, ulen, udiff, on tiny dataset
-        "BCR=86237 GEX=85679 LVARSP=gex_med,CD19_ab,CD25_ab,IGLV3-1_g,RPS27_g CELLS=3 FULL_SEQC \
-         CVARSP=ulen,udiff",
-        // 5. tests TCR and correct grouping of onesies on AGBT Donor 2 dataset
-        "TCR=101287 MIN_CELLS=100",
-        // 6. tests AMINO=
-        "BCR=86237 CELLS=3 AMINO= CVARS=umed,rmed,cdr3_dna",
-        // 7. tests SHM deletion
-        "BCR=123085 CVARSP=var,clen,cdiff CDR3=CAREPLYYDFWSAYFDYW LVARSP=near,far",
-        // 8. this clonotype included a junk chain before we made a change
-        "TCR=163911 CDR3=CAPSAGDKIIF AMINO=donor",
-        // 9. tests PER_BC
-        "BCR=85333 CDR3=CAKGDRTGYSYGGGIFDYW PER_BC",
-        // 10. tests multiple datasets and also LVARS=ncells,donors,datasets, and share
-        // Note that we have deliberately "faked" two donors.  In reality there is one.
-        "BCR=\"123085;123089\" CDR3=CVKDRVTGTITELDYW LVARS=ncells,donors,datasets AMINO=share NDONOR",
-        // 11. tests META
-        "META=test/inputs/meta_test11 CDR3=CARSFFGDTAMVMFQAFDPW LVARSP=donors,gex_med",
-        // 12. this added because it got better when a noise filter was added, also tests umax
-        "TCR=163914 CDR3=CASSLVQPSTDTQYF CVARSP=umax",
-        // 13. this added because it got better when a noise filter was added; also test FASTA
-        "TCR=163914 CDR3=CAFRGGSYIPTF FASTA=stdout",
-        // 14. this added because it got better when a bug in bads detection was fixed
-        "TCR=163914 CDR3=CASRLGGEETQYF",
-        // 15. tests insertion
-        "BCR=86233 CDR3=CVTEGKGDSVYLEKW CVARS=notes AMINO=cdr3,var",
-        // BCR=123085 CDR3=CARHPAPNYGFWSGYYKTDNWFDPW ==> alt example if we need to dump 86233
-        // 16. tests number of cells broken out by dataset
-        "BCR=123085,123089 LVARS=ncells,n_123085,n_123089 CDR3=CTRDRDLRGATDAFDIW",
-        // 17. tests gex with PER_BC and tests n_gex
-        // See also enclone_test_prebuild below, that tests nearly the same thing,
-        // and tests versus the same output file.
-        "BCR=86237 GEX=85679 LVARSP=gex_max,gex_med,n_gex,CD19_ab CELLS=3 PER_BC",
-        // 18. makes sure cross filtering is isn't applied to two samples from same donor
-        "BCR=123085:123089 CDR3=CVRDEGGARPNKWNYEGAFDIW",
-        // 19. there was a bug that caused twosie to be deleted, and there was foursie junk
-        "BCR=123085 CDR3=CARRYFGVVADAFDIW",
-        // 20. example affected by whitelist (gel bead oligo contamination) filtering
-        "BCR=52177 AMINO=cdr3 PER_BC CDR3=CATWDDSLSGPNWVF",
-        // 21. test MIN_CHAINS_EXACT
-        "BCR=123089 CDR3=CGTWHSNSKPNWVF MIN_CHAINS_EXACT=3",
-        // 22. there was a FP
-        "BCR=\"165807;165808\" FAIL_ONLY=true EXPECT_NULL",
-        // 23. here we were generating a fake alternate allele
-        "BCR=83808 CDR3=CAREGRGMVTTNPFDYW MIN_CELLS_EXACT=30",
-        // 24. an example that uses IGHE
-        "BCR=52177 CDR3=CSTGWGLDFDFWSGYYTAGYHW",
-        // 25. add mouse B6 example that had messed up constant regions
-        "TCR=74396 MOUSE CVARSP=cdiff CDR3=CASSDAGDTQYF",
-        // 26. tests multiple datasets and also LVARS=ncells,donors,datasets, and share
-        // Note that we have deliberately "faked" two donors.  In reality there is one.
-        // Here we make sure that non-specification of NDONOR works.
-        "BCR=\"123085;123089\" CDR3=CVKDRVTGTITELDYW",
-        // 27. tests SUMMARY and NOPRINT
-        "BCR=123085 SUMMARY SUMMARY_CLEAN NOPRINT",
-        // 28. tests BARCODE option
-        "BCR=165807 BARCODE=CCCATACGTGATGATA-1,TCTATTGAGCTGAAAT-1",
-        // 29. tests parenthesized variable in F, SUM and MEAN
-        "BCR=86237 GEX=85679 LVARSP=IGHV3-7_g F=\"(IGHV3-7_g)>=4.5\" MIN_CHAINS=2 SUM MEAN",
-        // 30. tests d_univ and d_donor
-        "BCR=123085 CVARSP=d_univ,d_donor CDR3=CVKDRVTGTITELDYW",
-        // 31. tests Cell Ranger 3.1 output
-        "BCR=../3.1/123085 CDR3=CVKDRVTGTITELDYW",
-        // 32. tests Cell Ranger 2.0 output and RE
-        "BCR=../2.0/124550 CDR3=CAREPLYYDFWSAYFDYW RE",
-    ];
     //                       id    ok   output
     let mut results = Vec::<(usize, bool, String)>::new();
-    for i in 0..tests.len() {
+    for i in 0..TESTS.len() {
         results.push((i, false, String::new()));
     }
     results.par_iter_mut().for_each(|res| {
         let it = res.0;
-        let mut test = tests[it].to_string();
+        let mut test = TESTS[it].to_string();
         let mut expect_null = false;
         if test.contains(" EXPECT_NULL") {
             test = test.replace(" EXPECT_NULL", "");
@@ -286,7 +295,7 @@ fn test_enclone() {
     }
     println!(
         "\ntotal time for {} enclone subtests = {:.2} seconds\n",
-        tests.len(),
+        TESTS.len(),
         elapsed(&t)
     );
 }
@@ -402,10 +411,10 @@ fn test_proto_write() -> Result<(), Error> {
     let pre_arg = format!("PRE=test/inputs/version{}", TEST_FILES_VERSION);
     let binary_arg = format!("BINARY={}.bin", LOUPE_OUT_FILENAME);
     let proto_arg = format!("PROTO={}.proto", LOUPE_OUT_FILENAME);
-    for t in tests {
+    for t in tests.iter() {
         // FIXME: It would be nicer to use the enclone API here
         std::process::Command::new("target/release/enclone")
-            .args(&[&pre_arg, t, &binary_arg, &proto_arg])
+            .args(&[&pre_arg, *t, &binary_arg, &proto_arg])
             .output()
             .expect(&format!("failed to execute enclone for test_proto_write"));
         let outputs_proto = read_proto(format!("{}.proto", LOUPE_OUT_FILENAME))?;
