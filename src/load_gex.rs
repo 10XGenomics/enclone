83bf068f8470cebc158816c3f123d2006ed36713
David Jaffe <david.jaffe@10xgenomics.com>
2020-03-01 09:05:38 -0800

    improve handling of an error

--- src/load_gex.rs
+++ src/load_gex.rs
@@ -390,12 +390,8 @@ pub fn get_gex_info(mut ctl: &mut EncloneControl) -> GexInfo {
                     f = format!("{}/raw_gene_bc_matrices_h5.h5", gex_outs[i]);
                 }
                 if !path_exists(&f) {
-                    panic!(
-                        "Something has gone badly wrong, the h5 file\n\
-                         {}\n\
-                         does not exist.",
-                        f
-                    );
+                    eprintln!("\nThere's a missing input file:\n{}.\n", f);
+                    std::process::exit(1);
                 }
                 let h = h5::File::open(&f, "r").unwrap();
                 h5_data.push(Some(h.dataset("matrix/data").unwrap()));
