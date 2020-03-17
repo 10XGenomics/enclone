d85d5111d41ef5cc3126e3c35a11f2ba96e15ae0
David Jaffe <david.jaffe@10xgenomics.com>
2020-03-16 18:45:40 -0700

    some pretty formatting

--- src/help4.rs
+++ src/help4.rs
@@ -448,10 +448,10 @@ pub fn help4(args: &Vec<String>) {
 
 
 
-        print(
+        print!( "{}", gray_left_bar(&print_to(
             "\\red{◉} These variables have some alternate versions, \
-             as shown in the table below:\n"
-        );
+             as shown in the table below:\n\n"
+        )));
         let mut rows = Vec::<Vec<String>>::new();
         let row = vec![ 
             "variable".to_string(),
@@ -548,9 +548,9 @@ pub fn help4(args: &Vec<String>) {
         {
             let mut log = String::new();
             print_tabular_vbox(&mut log, &rows, 2, &b"l|l|l|l|l|l".to_vec(), false, false);
-            print!("{}", log);
+            print!("{}", gray_left_bar(&log));
         }
-        print(
+        print!( "{}", gray_left_bar(&print_to(
             "Some explanation is required.  If you use enclone without certain options, you \
              get the \"visual\" column.\n\
              • Add the option \\bold{PER_CELL} \
@@ -564,10 +564,10 @@ pub fn help4(args: &Vec<String>) {
              visual output (to save space), and optionally, in names of fields on the command \
              line.\n\
              \\green{▶} If you try out these features, you'll see exactly what happens! \
-             \\green{◀}\n\n"
-        );
+             \\green{◀}\n"
+        )));
 
-
+        println!("");
 
         print_with_box(
             "\\red{◉} If used with \\bold{PER_CELL} option (see \"enclone help display\"), for \
