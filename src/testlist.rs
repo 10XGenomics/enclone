e683d8d0d2910af9239872146d79890562edaa59
David Jaffe <david.jaffe@10xgenomics.com>
2020-03-20 08:40:48 -0700

    add to test

--- src/testlist.rs
+++ src/testlist.rs
@@ -18,8 +18,8 @@ pub const TESTS: [&str; 46] = [
      CVARSP=ulen,udiff"###,
     // 5. tests TCR and correct grouping of onesies on AGBT Donor 2 dataset
     r###"TCR=101287 MIN_CELLS=100"###,
-    // 6. tests AMINO=
-    r###"BCR=86237 CELLS=3 AMINO= CVARS=u,r,cdr3_dna"###,
+    // 6. tests AMINO= and vjlen
+    r###"BCR=86237 CELLS=3 AMINO= CVARS=u,r,cdr3_dna,vjlen"###,
     // 7. tests SHM deletion
     r###"BCR=123085 CVARSP=var,clen,cdiff CDR3=CAREPLYYDFWSAYFDYW LVARSP=near,far"###,
     // 8. this clonotype included a junk chain before we made a change
