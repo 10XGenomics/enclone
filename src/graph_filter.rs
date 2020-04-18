d9d44912b26bd57a26e4c7e7cc9effd90cb90d09
David Jaffe <david.jaffe@10xgenomics.com>
2020-04-18 06:09:58 -0700

    add to logging produced by internal option

--- src/graph_filter.rs
+++ src/graph_filter.rs
@@ -29,6 +29,7 @@ use vector_utils::*;
 // Hmm, seems like the edges go from heavy to light.
 
 pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
+    let mut ndels = 0;
     let mut seqs = Vec::<(Vec<u8>, bool, String, usize)>::new();
     for i in 0..tig_bc.len() {
         for j in 0..tig_bc[i].len() {
@@ -146,7 +147,7 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
                             let w = stats[i].2;
                             if graph {
                                 println!(
-                                    "\nkill type 2, from {} to {}, ncells = {}, numi = {}",
+                                    "\nkill type 1, from {} to {}, ncells = {}, numi = {}",
                                     seqs[v].2, seqs[w].2, ncells, numi
                                 );
                                 println!(
@@ -165,6 +166,16 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
                                     && ncellsx >= MIN_RATIO_KILL * ncells
                                     && numix >= MIN_RATIO_KILL * numi
                                 {
+                                    if graph {
+                                        println!(
+                                            "\nkill type 2, from {} to {}, ncells = {}, numi = {}",
+                                            seqs[v].2, seqs[w].2, ncells, numi
+                                        );
+                                        println!(
+                                            "killed by {} to {}, ncells = {}, numi = {}",
+                                            seqs[v].2, seqs[stats[0].2].2, ncells_best, numi_best
+                                        );
+                                    }
                                     res.1.push((v, w));
                                 }
                             }
@@ -221,6 +232,9 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
     });
     for i in 0..tig_bc.len() {
         to_delete[i] = results[i].1;
+        if to_delete[i] {
+            ndels += 1;
+        }
     }
     erase_if(&mut tig_bc, &to_delete);
     if graph {
@@ -231,7 +245,7 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
     // Kill weak branches from heavy to light chains.
 
     let mut log = Vec::<u8>::new();
-    fwriteln!(log, "BRANCHING FROM HEAVY CHAINS");
+    fwriteln!(log, "\nBRANCHING FROM HEAVY CHAINS");
     const MIN_RATIO_KILL_HEAVY: usize = 8;
     const MAX_KILL_HEAVY: usize = 6;
     const MAX_KILL_HEAVY_CELLS: usize = 1;
@@ -266,6 +280,13 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
                     && (stats[i].0).0 <= MAX_KILL_HEAVY
                     && (stats[i].0).1 <= MAX_KILL_HEAVY_CELLS
                 {
+                    if graph {
+                        let w = stats[i].1;
+                        println!(
+                            "\nkill type 3, from {} to {}\nkilled by {} to {}",
+                            seqs[v].2, seqs[w].2, seqs[v].2, seqs[stats[0].1].2
+                        );
+                    }
                     res.1.push((v, stats[i].1));
                 }
             }
@@ -306,10 +327,14 @@ pub fn graph_filter(mut tig_bc: &mut Vec<Vec<TigData>>, graph: bool) {
     });
     for i in 0..tig_bc.len() {
         to_delete[i] = results[i].1;
+        if to_delete[i] {
+            ndels += 1;
+        }
     }
     erase_if(&mut tig_bc, &to_delete);
     if graph {
         fwriteln!(log, "");
         print!("{}", strme(&log));
+        println!("total graph filter deletions = {}", ndels);
     }
 }
