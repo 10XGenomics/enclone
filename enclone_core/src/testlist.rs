d95402d340ea9d4638dab40c6d4cdd11b355c9d1
David Jaffe <david.jaffe@10xgenomics.com>
2020-05-07 09:40:05 -0700

    add test (but there's a problem)

--- enclone_core/src/testlist.rs
+++ enclone_core/src/testlist.rs
@@ -24,7 +24,7 @@ pub fn enclone_testdata_public_gex_human() -> String {
 
 pub const TEST_FILES_VERSION: u8 = 14;
 
-pub const TESTS: [&str; 72] = [
+pub const TESTS: [&str; 73] = [
     // 1. tests variant base after CDR3, parseable output
     r###"BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout
      PCOLS=exact_subclonotype_id,n,v_name1,v_name2,nchains,var_indices_aa1,barcodes"###,
@@ -200,6 +200,8 @@ pub const TESTS: [&str; 72] = [
         CDR3=CARSFFGDTAMVMFQAFDPW LVARSP=donors,gex NO_PRE"###,
     // 72. test SUMMARY_CSV
     r###"BCR=86237 NOPRINT SUMMARY_CSV"###,
+    // 73. this crashed before a bug was fixed
+    r###"BCR=1021341 NCELL CDR3=CQQANSYPLTF SEG=IGHV1-69D"###,
 ];
 
 // List of examples in documentation.
