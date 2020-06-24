ddf57afb092a841ab7315e11dd3d26b3ea42ef25
Wyatt McDonnell <18267139+wyattmcdonnell@users.noreply.github.com>
2020-06-24 12:15:42 -0700

    dictionary exceptions

--- enclone_main/tests/enclone_test.rs
+++ enclone_main/tests/enclone_test.rs
@@ -678,7 +678,7 @@ fn test_for_broken_links_and_spellcheck() {
         genomics germline github githubusercontent google grok gz html \
         hypermutation hypermutations igh ighm igkc imgt \
         indel indels inkt json levenshtein linux loh lvars macbook mait metadata mkdir \
-        moresies multiomic ncbi ncross \
+        moresies multiomic ncbi ncross NEWICK Newick \
         nopager noprint nqual nwhitef oligos onesie parseable pbmc pcell phylip plasmablast \
         preinstalled prepends redownloads screenshot segn sloooooooow \
         spacebar stackexchange standalone stdout subclonotype \
