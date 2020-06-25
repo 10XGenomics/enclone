1865c1ac6d633dedbc2e536e34d2a9e3e6b567f9
David Jaffe <david.jaffe@10xgenomics.com>
2020-06-25 12:59:33 -0700

    fix issues

--- enclone/src/bin/sync_to_master.rs
+++ enclone/src/bin/sync_to_master.rs
@@ -7,7 +7,8 @@ use io_utils::*;
 use itertools::Itertools;
 use pretty_trace::*;
 use std::collections::HashMap;
-use std::fs::read_dir;
+use std::fs::{File, read_dir};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use string_utils::*;
 
 fn main() {
@@ -16,7 +17,9 @@ fn main() {
     let f = open_for_read!["master.toml"];
     for line in f.lines() {
         let s = line.unwrap();
-        version.insert(s.before(" = ").to_string(), s.after(" = ").to_string());
+        if !s.starts_with('#') && s.contains("=") {
+            version.insert(s.before(" = ").to_string(), s.after(" = ").to_string());
+        }
     }
     let all = read_dir(".").unwrap();
     for f in all {
@@ -31,15 +34,15 @@ fn main() {
                     let s = line.unwrap();
                     let mut t = s.clone();
                     if s.contains(" =") {
-                        let crate = s.before(" =").to_string();
-                        if version.contains_key(&crate) {
-                            t = format!("{} = {}", crate, version[crate]);
+                        let cratex = s.before(" =").to_string();
+                        if version.contains_key(&cratex) {
+                            t = format!("{} = {}", cratex, version[&cratex]);
                         }
                     }
                     newg.push(t);
                 }
             }
-            let mut g = open_for_write[&toml];
+            let mut g = open_for_write_new![&toml];
             fwriteln!(g, "{}\n", newg.iter().format("\n"));
         }
     }
