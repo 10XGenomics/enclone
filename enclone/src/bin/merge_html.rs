a31251b75c60f91da2ea949cb8051d115b0df6bc
David Jaffe <david.jaffe@10xgenomics.com>
2020-07-01 07:41:27 -0700

    finish propagating enclone formatting

--- enclone/src/bin/merge_html.rs
+++ enclone/src/bin/merge_html.rs
@@ -13,6 +13,13 @@ fn main() {
     insert_html("pages/tree.html.src", "pages/auto/tree.html", false);
     insert_html("pages/plot.html.src", "pages/auto/plot.html", false);
     insert_html("pages/windows.html.src", "pages/auto/windows.html", false);
+    insert_html("pages/history.html.src", "pages/auto/history.html", false);
+    insert_html("pages/compile.html.src", "pages/auto/compile.html", false);
+    insert_html(
+        "pages/dang_i_cannot_install.html.src",
+        "pages/auto/dang_i_cannot_install.html",
+        false,
+    );
     insert_html(
         "pages/installation_details.html.src",
         "pages/auto/installation_details.html",
