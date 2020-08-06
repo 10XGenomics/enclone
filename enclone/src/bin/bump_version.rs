e1cae52726c263b4bcc9de702e4391fc24fa2e4c
David Jaffe <david.jaffe@10xgenomics.com>
2020-08-06 05:39:54 -0700

    add code to bump version for release

---
+++ enclone/src/bin/bump_version.rs
@@ -0,0 +1,72 @@
+// Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
+
+// Bump version x.y.z to x.y.z+1 in every Cargo.toml.
+
+use io_utils::*;
+use itertools::Itertools;
+use pretty_trace::*;
+use std::fs::{read_dir, File};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use string_utils::*;
+use vector_utils::*;
+
+fn main() {
+    PrettyTrace::new().on();
+    let all = read_dir(".").unwrap();
+    let mut versions = Vec::<String>::new();
+    for f in all {
+        let f = f.unwrap().path();
+        let f = f.to_str().unwrap();
+        let toml = format!("{}/Cargo.toml", f);
+        if path_exists(&toml) {
+            let g = open_for_read![&toml];
+            let mut found_version = false;
+            for line in g.lines() {
+                let s = line.unwrap();
+                if s.starts_with("version = \"") {
+                    versions.push(s.between("\"", "\"").to_string());
+                    found_version = true;
+                }
+            }
+            if !found_version {
+                eprintln!("\nFailed to find version in {}.\n", toml);
+                std::process::exit(1);
+            }
+        }
+    }
+    unique_sort(&mut versions);
+    if versions.len() > 1 {
+        eprintln!(
+            "\nFound multiple versions: {}",
+            versions.iter().format(", ")
+        );
+        std::process::exit(1);
+    }
+    let version = format!(
+        "{}.{}",
+        versions[0].rev_before("."),
+        versions[0].rev_after(".").force_usize() + 1
+    );
+    let all = read_dir(".").unwrap();
+    for f in all {
+        let f = f.unwrap().path();
+        let f = f.to_str().unwrap();
+        let toml = format!("{}/Cargo.toml", f);
+        if path_exists(&toml) {
+            let mut newg = Vec::<String>::new();
+            {
+                let g = open_for_read![&toml];
+                for line in g.lines() {
+                    let s = line.unwrap();
+                    if s.starts_with("version = ") {
+                        newg.push(format!("version = \"{}\"", version));
+                    } else {
+                        newg.push(s.clone());
+                    }
+                }
+            }
+            let mut g = open_for_write_new![&toml];
+            fwrite!(g, "{}\n", newg.iter().format("\n"));
+        }
+    }
+}
