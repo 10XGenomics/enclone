8d0e6b606c2be0b56726cbaef9ea89b63569a371
David Jaffe <david.jaffe@10xgenomics.com>
2020-08-17 15:11:49 -0700

    do more of the release process

--- enclone/src/bin/bump_version.rs
+++ enclone/src/bin/bump_version.rs
@@ -1,17 +1,23 @@
 // Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
 
-// Bump version x.y.z to x.y.z+1 in every Cargo.toml.
+// Start a release, assuming that you want to bump the z in x.y.z.
+//
+// See enclone/release_instructions.
 
 use io_utils::*;
 use itertools::Itertools;
 use pretty_trace::*;
 use std::fs::{read_dir, File};
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::process::Command;
 use string_utils::*;
 use vector_utils::*;
 
 fn main() {
     PrettyTrace::new().on();
+
+    // Step 1.  Bump version x.y.z to x.y.z+1 in every Cargo.toml.
+
     let all = read_dir(".").unwrap();
     let mut versions = Vec::<String>::new();
     for f in all {
@@ -42,6 +48,7 @@ fn main() {
         );
         std::process::exit(1);
     }
+    let old_version = versions[0].clone();
     let version = format!(
         "{}.{}",
         versions[0].rev_before("."),
@@ -69,4 +76,62 @@ fn main() {
             fwrite!(g, "{}\n", newg.iter().format("\n"));
         }
     }
+
+    // 2. Edit README.md to reflect the upcoming version.
+
+    {
+        let readme = include_str!["../../../README.md"].replace(&old_version, &version);
+        let mut g = open_for_write_new!["README.md"];
+        fwriteln!(g, "{}", readme);
+    }
+
+    // 3. Commit and push changes.
+
+    let new = Command::new("git")
+        .arg("commit")
+        .arg("-a")
+        .output()
+        .expect(&format!("failed to execute git commit"));
+    if new.status.code() != Some(0) {
+        eprintln!("\ngit commit failed\n");
+        std::process::exit(1);
+    }
+    let new = Command::new("git")
+        .arg("push")
+        .output()
+        .expect(&format!("failed to execute git push"));
+    if new.status.code() != Some(0) {
+        eprintln!("\ngit push failed\n");
+        std::process::exit(1);
+    }
+
+    // 4. Tag the commit.
+
+    let new = Command::new("git")
+        .arg("tag")
+        .arg(&version)
+        .output()
+        .expect(&format!("failed to execute git tag"));
+    if new.status.code() != Some(0) {
+        eprintln!("\ngit tag failed\n");
+        std::process::exit(1);
+    }
+
+    // 5. Trigger the release.
+
+    let new = Command::new("git")
+        .arg("push")
+        .arg("origin")
+        .arg("--tags")
+        .output()
+        .expect(&format!("failed to trigger release"));
+    if new.status.code() != Some(0) {
+        eprintln!("\nattempt to trigger release failed\n");
+        std::process::exit(1);
+    }
+
+    // 6. Done.
+
+    println!("\nAll done, looks like it worked!\n");
+    println!("Please read enclone/release_instructions.\n");
 }
