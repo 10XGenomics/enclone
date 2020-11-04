4ffbc151a2933a03b35dc7e9610635e98e13e184
David Jaffe <david.jaffe@10xgenomics.com>
2020-11-04 11:32:51 -0800

    attempt to fix test

--- enclone_main/tests/enclone_test.rs
+++ enclone_main/tests/enclone_test.rs
@@ -1715,7 +1715,7 @@ fn test_annotated_example() {
 #[cfg(not(feature = "cpu"))]
 #[test]
 fn test_subset_json() {
-    let _ = std::fs::create_dir("testx/outputs/woof");
+    std::fs::create_dir_all("testx/outputs/woof").unwrap();
     let test = "BCR=123085 CDR3=CARVGSFLSSSWHPRDYYYYGMDVW \
         SUBSET_JSON=testx/outputs/woof/all_contig_annotations.json";
     let args = parse_bsv(&test);
