b364389118c2260b2d54073e44677ffe43ed853a
David Jaffe <david.jaffe@10xgenomics.com>
2020-12-11 06:08:15 -0800

    set up interface for asymmetric grouping

--- enclone/src/proc_args.rs
+++ enclone/src/proc_args.rs
@@ -342,6 +342,7 @@ pub fn proc_args(mut ctl: &mut EncloneControl, args: &Vec<String>) {
         ("ACCEPT_BROKEN", &mut ctl.gen_opt.accept_broken),
         ("ACCEPT_INCONSISTENT", &mut ctl.gen_opt.accept_inconsistent),
         ("ACCEPT_REUSE", &mut ctl.gen_opt.accept_reuse),
+        ("AGROUP", &mut ctl.clono_group_opt.asymmetric),
         ("ALLOW_INCONSISTENT", &mut ctl.gen_opt.allow_inconsistent),
         ("ANN", &mut ctl.join_print_opt.ann),
         ("ANN0", &mut ctl.join_print_opt.ann0),
@@ -468,6 +469,15 @@ pub fn proc_args(mut ctl: &mut EncloneControl, args: &Vec<String>) {
     // Define arguments that set something to a string.
 
     let set_string = [
+        ("AG_CENTER", &mut ctl.clono_group_opt.asymmetric_center),
+        (
+            "AG_DIST_BOUND",
+            &mut ctl.clono_group_opt.asymmetric_dist_bound,
+        ),
+        (
+            "AG_DIST_FORMULA",
+            &mut ctl.clono_group_opt.asymmetric_dist_formula,
+        ),
         ("CLUSTAL_AA", &mut ctl.gen_opt.clustal_aa),
         ("CLUSTAL_DNA", &mut ctl.gen_opt.clustal_dna),
         ("EXT", &mut ctl.gen_opt.ext),
@@ -1083,7 +1093,97 @@ pub fn proc_args(mut ctl: &mut EncloneControl, args: &Vec<String>) {
         **f = stringme(&tilde_expand(&f.as_bytes()));
     }
 
-    // Sanity check arguments (and more below).
+    // Sanity check grouping arguments.
+
+    let mut group_styles = 0;
+    if ctl.clono_group_opt.heavy_cdr3_aa {
+        group_styles += 1;
+    }
+    if ctl.clono_group_opt.vj_refname {
+        group_styles += 1;
+    }
+    if ctl.clono_group_opt.vj_refname_strong {
+        group_styles += 1;
+    }
+    if ctl.clono_group_opt.asymmetric {
+        group_styles += 1;
+    }
+    if group_styles > 1 {
+        eprintln!(
+            "\nOnly one of the options\n\
+            GROUP_HEAVY_CDR3, GROUP_VJ_REFNAME, GROUP_VJ_REFNAME_STRONG, AGROUP\n\
+            may be specified at a time.\n"
+        );
+        std::process::exit(1);
+    }
+    if ctl.clono_group_opt.asymmetric {
+        if ctl.clono_group_opt.asymmetric_center.len() == 0
+            || ctl.clono_group_opt.asymmetric_dist_formula.len() == 0
+            || ctl.clono_group_opt.asymmetric_dist_bound.len() == 0
+        {
+            eprintln!(
+                "\nIf the AGROUP option is used to specify asymmetric grouping, then all\n\
+                of the options AG_CENTER, AG_DIST_FORMULA and AG_DIST_BOUND must also be \
+                specified.\n"
+            );
+            std::process::exit(1);
+        }
+    }
+    if ctl.clono_group_opt.asymmetric_center.len() > 0
+        || ctl.clono_group_opt.asymmetric_dist_formula.len() > 0
+        || ctl.clono_group_opt.asymmetric_dist_bound.len() > 0
+    {
+        if !ctl.clono_group_opt.asymmetric {
+            eprintln!(
+                "\nIf any of the asymmetric grouping options AG_CENTER or \
+                    AG_DIST_FORMULA or\nAG_DIST_BOUND are specified, then the option AGROUP \
+                    must also be specified, to turn on asymmetric grouping.\n"
+            );
+        }
+        std::process::exit(1);
+    }
+    if ctl.clono_group_opt.asymmetric {
+        if !ctl
+            .clono_group_opt
+            .asymmetric_center
+            .starts_with("min_cells=")
+            || !ctl
+                .clono_group_opt
+                .asymmetric_center
+                .after("min_cells=")
+                .parse::<usize>()
+                .is_ok()
+        {
+            eprintln!(
+                "\nThe only allowed form for AG_CENTER is AG_CENTER=min_cells=n, \
+                where n is an integer.\n"
+            );
+            std::process::exit(1);
+        }
+        if ctl.clono_group_opt.asymmetric_dist_formula != "cdr3_edit_distance" {
+            eprintln!("\nThe only allowed form for AG_DIST_FORMULA is cdr3_edit_distance.\n");
+            std::process::exit(1);
+        }
+        if !ctl
+            .clono_group_opt
+            .asymmetric_dist_bound
+            .starts_with("top=")
+            || !ctl
+                .clono_group_opt
+                .asymmetric_dist_bound
+                .after("top=")
+                .parse::<usize>()
+                .is_ok()
+        {
+            eprintln!(
+                "\nThe only allowed form for AG_DIST_BOUND is top=n, where n is an \
+                integer.\n"
+            );
+            std::process::exit(1);
+        }
+    }
+
+    // Sanity check other arguments (and more below).
 
     if ctl.clono_filt_opt.cdr3.is_some() && ctl.clono_filt_opt.cdr3_lev.len() > 0 {
         eprintln!(
