41c3c1135cea4f57b79b34f4a0d8ad738cabe489
David Jaffe <david.jaffe@10xgenomics.com>
2021-01-07 07:01:31 -0800

    reduce computational complexity of honeycomb plot generation

--- enclone_tail/src/plot.rs
+++ enclone_tail/src/plot.rs
@@ -118,11 +118,52 @@ fn hex_coord(n: usize, r: f64) -> (f64, f64) {
 // circles.  There is probably a literature on this, and this is probably a very crappy algorithm.
 
 fn pack_circles(r: &Vec<f64>) -> Vec<(f64, f64)> {
+    // Set up to track the centers of the circles.
+
     let mut c = Vec::<(f64, f64)>::new();
     if r.is_empty() {
         return c;
     }
-    c.push((0.0, 0.0));
+
+    // Set up to track the x extents of the circles, which are intervals, and which we break into
+    // subintervals equal to the extent of the smallest circle (and a possible fractional part).
+    // We track the starting point of each subinterval, and the index of the circle.
+    //
+    // The overall code would presumably be faster if we did not store the data in a sorted
+    // vector that needs to be sorted after each center is added.
+
+    let mut ints = Vec::<(f64, usize)>::new();
+    let radius0 = *r.last().unwrap();
+
+    // Define function to add a center.
+
+    fn push_center(
+        x: f64,
+        y: f64,
+        index: usize,
+        radius: f64,
+        radius0: f64,
+        c: &mut Vec<(f64, f64)>,
+        ints: &mut Vec<(f64, usize)>,
+    ) {
+        c.push((x, y));
+
+        // need to cover [x-radius..x+radius] using segments of length radius0
+
+        let mut pos = x - radius;
+        while pos < x + radius {
+            ints.push((pos, index));
+            pos += radius0;
+        }
+        ints.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    }
+
+    // Define data for the first circle.
+
+    push_center(0.0, 0.0, 0, r[0], radius0, &mut c, &mut ints);
+
+    // Proceed.
+
     let mut bigr = r[0];
     let mut rand = 0i64;
     // We use a ridiculously large sample.  Reducing it to 1000 substantially reduces symmetry.
@@ -140,6 +181,7 @@ fn pack_circles(r: &Vec<f64>) -> Vec<(f64, f64)> {
                 // Get a random point in [-1,+1] x [-1,+1].  Using a hand-rolled random number
                 // generator (from the internet) for speed and reproducibility, although there
                 // might be something better in the standard packages.
+
                 let rand1 = 6_364_136_223_846_793_005i64
                     .wrapping_mul(rand)
                     .wrapping_add(1_442_695_040_888_963_407);
@@ -149,12 +191,33 @@ fn pack_circles(r: &Vec<f64>) -> Vec<(f64, f64)> {
                 rand = rand2;
                 let mut r1 = (2.0 * (rand1 % 1_000_000i64) as f64 / 1_000_000.0) - 1.0;
                 let mut r2 = (2.0 * (rand2 % 1_000_000i64) as f64 / 1_000_000.0) - 1.0;
+
                 // Make it bigger.
+
                 r1 *= (bigr + r[i]) * MUL;
                 r2 *= (bigr + r[i]) * MUL;
+
                 // See if circle at (r1,r2) overlaps any of the existing circles.
+                // This involves a binary search, which reduces the complexity of the algorithm.
+
                 let mut ok = true;
-                for k in 0..i {
+                let low = ints.binary_search_by(|v| {
+                    v.partial_cmp(&(r1 - r[i] - radius0, 0))
+                        .expect("Comparison failed.")
+                });
+                let low = match low {
+                    Ok(i) => i,
+                    Err(i) => i,
+                };
+                let high = ints.binary_search_by(|v| {
+                    v.partial_cmp(&(r1 + r[i], 0)).expect("Comparison failed.")
+                });
+                let high = match high {
+                    Ok(i) => i,
+                    Err(i) => i,
+                };
+                for m in low..high {
+                    let k = ints[m].1;
                     let d = (c[k].0 - r1) * (c[k].0 - r1) + (c[k].1 - r2) * (c[k].1 - r2);
                     if d < (r[i] + r[k]) * (r[i] + r[k]) {
                         ok = false;
@@ -175,7 +238,7 @@ fn pack_circles(r: &Vec<f64>) -> Vec<(f64, f64)> {
                 break;
             }
         }
-        c.push((best_r1, best_r2));
+        push_center(best_r1, best_r2, i, r[i], radius0, &mut c, &mut ints);
         bigr = bigr.max(r[i] + (c[i].0 * c[i].0 + c[i].1 * c[i].1).sqrt());
     }
     c
