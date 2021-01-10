3fd322761089339204a3413b7c138e1748e08b50
Wyatt McDonnell <18267139+wyattmcdonnell@users.noreply.github.com>
2021-01-10 12:36:06 -0800

    Update dictionary, pass tests, fmt

--- enclone_main/tests/enclone_test.rs
+++ enclone_main/tests/enclone_test.rs
@@ -896,7 +896,7 @@ fn test_for_broken_links_and_spellcheck() {
         indel indels inkt jsdelivr json levenshtein linux loh lvars macbook mait metadata mkdir \
         moresies multiomic nall ncbi ncross ndoublet newick nimproper \
         nopager noprint nqual nwhitef oligos onesie onesies parseable pbmc pcell pdb phylip \
-        plasmablast preinstalled prepends pwm pwms redownloads samtools screenshot segn \
+        plasmablast preinstalled prepends pwm pwms redownloads researchsquare samtools screenshot segn \
         sloooooooow spacebar stackexchange standalone stdout subclonotype \
         subclonotypes svg thresholding timepoint tracebacks trb twosie ubuntu \
         umi umis underperforming unicode untarring vdj website wget wikimedia \
