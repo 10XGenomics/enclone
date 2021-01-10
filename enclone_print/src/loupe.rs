903a7c5170f86f68dda90d64159daa98ae28de92
Wyatt McDonnell <18267139+wyattmcdonnell@users.noreply.github.com>
2021-01-10 11:10:56 -0800

    Update enclone_print (c)

--- enclone_print/src/loupe.rs
+++ enclone_print/src/loupe.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
+// Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 //
 // This set of functions writes a protobuf data structure that
 // Loupe uses to access clonotype data.
--- enclone_print/src/print_utils2.rs
+++ enclone_print/src/print_utils2.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
+// Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
 // This file contains the single function row_fill,
 // plus a small helper function get_gex_matrix_entry.
