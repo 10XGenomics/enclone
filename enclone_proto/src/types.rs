8ea7d0298f57123408136c8f931bdcb44c810871
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2021-01-15 13:24:22 -0800

    Simplify region fns

--- enclone_proto/src/types.rs
+++ enclone_proto/src/types.rs
@@ -4,6 +4,7 @@
 //! This crate defines the data structure that would represent the clonotypes
 //! computed by enclone.
 //!
+use vdj_ann::annotate::Region as AnnRegion;
 
 include!(concat!(env!("OUT_DIR"), "/enclone.types.rs"));
 
@@ -16,119 +17,87 @@ impl From<&bio::alignment::Alignment> for Alignment {
     }
 }
 
+fn make_ann_region(
+    start: Option<u32>,
+    end: Option<u32>,
+    nt_seq: &[u8],
+    v_start: u32,
+    aa_seq: &[u8],
+) -> Option<AnnRegion> {
+    let v = v_start as usize;
+    match (start, end) {
+        (Some(s), Some(e)) => {
+            let s = s as usize;
+            let e = e as usize;
+            Some(AnnRegion {
+                start: s,
+                stop: e,
+                nt_seq: std::str::from_utf8(&nt_seq[s..e]).unwrap().to_string(),
+                aa_seq: std::str::from_utf8(&aa_seq[(s - v) / 3..(e - v) / 3])
+                    .unwrap()
+                    .to_string(),
+            })
+        }
+        _ => None,
+    }
+}
+
 impl ClonotypeChain {
-    pub fn fwr1_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr1_start.is_none() || self.cdr1_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.fwr1_start.unwrap() as usize..self.cdr1_start.unwrap() as usize]
-                .to_vec(),
+    pub fn fwr1_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr1_start,
+            self.cdr1_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn fwr1_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr1_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr1_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr1_start.is_none() || self.cdr1_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr1_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr1_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr1_nt(&self) -> Option<Vec<u8>> {
-        if self.cdr1_start.is_none() || self.fwr2_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.cdr1_start.unwrap() as usize..self.fwr2_start.unwrap() as usize]
-                .to_vec(),
+    pub fn cdr1_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.cdr1_start,
+            self.fwr2_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn cdr1_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr1_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr1_aa(&self) -> Option<Vec<u8>> {
-        if self.cdr1_start.is_none() || self.fwr2_start.is_none() {
-            return None;
-        }
-        let start = (self.cdr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.fwr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn cdr1_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr1_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr2_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr2_start.is_none() || self.cdr2_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.fwr2_start.unwrap() as usize..self.cdr2_start.unwrap() as usize]
-                .to_vec(),
+    pub fn fwr2_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr2_start,
+            self.cdr2_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn fwr2_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr2_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr2_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr2_start.is_none() || self.cdr2_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr2_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr2_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr2_nt(&self) -> Option<Vec<u8>> {
-        if self.cdr2_start.is_none() || self.fwr3_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.cdr2_start.unwrap() as usize..self.fwr3_start.unwrap() as usize]
-                .to_vec(),
+    pub fn cdr2_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.cdr2_start,
+            self.fwr3_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn cdr2_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr2_nt().as_deref().unwrap().to_vec()).ok()
+    pub fn fwr3_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr3_start,
+            Some(self.cdr3_start),
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
+        )
     }
-    pub fn cdr2_aa(&self) -> Option<Vec<u8>> {
-        if self.cdr2_start.is_none() || self.fwr3_start.is_none() {
-            return None;
-        }
-        let start = (self.cdr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.fwr3_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
+    pub fn fwr4_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            Some(self.cdr3_end),
+            self.fwr4_end,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
+        )
     }
-    pub fn cdr2_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr2_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr3_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr3_start.is_none() {
-            return None;
-        }
-        Some(self.nt_sequence[self.fwr3_start.unwrap() as usize..self.cdr3_start as usize].to_vec())
-    }
-    pub fn fwr3_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr3_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr3_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr3_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr3_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr3_start - self.v_start) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr3_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr3_aa().as_deref().unwrap().to_vec()).ok()
-    }
+
     pub fn cdr3_nt(&self) -> &[u8] {
         &self.nt_sequence[self.cdr3_start as usize..self.cdr3_end as usize]
     }
@@ -142,141 +111,63 @@ impl ClonotypeChain {
     }
     pub fn cdr3_aa_string(&self) -> String {
         std::str::from_utf8(self.cdr3_aa()).unwrap().to_string()
-    }
-    pub fn fwr4_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr4_end.is_none() {
-            return None;
-        }
-        Some(self.nt_sequence[self.cdr3_end as usize..self.fwr4_end.unwrap() as usize].to_vec())
-    }
-    pub fn fwr4_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr4_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr4_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr4_end.is_none() {
-            return None;
-        }
-        let start = (self.cdr3_end as usize - self.v_start as usize) / 3;
-        let end = (self.fwr4_end.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr4_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr4_aa().as_deref().unwrap().to_vec()).ok()
     }
 }
 
 impl ExactSubClonotypeChain {
-    pub fn fwr1_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr1_start.is_none() || self.cdr1_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.fwr1_start.unwrap() as usize..self.cdr1_start.unwrap() as usize]
-                .to_vec(),
+    pub fn fwr1_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr1_start,
+            self.cdr1_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn fwr1_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr1_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr1_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr1_start.is_none() || self.cdr1_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr1_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr1_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr1_nt(&self) -> Option<Vec<u8>> {
-        if self.cdr1_start.is_none() || self.fwr2_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.cdr1_start.unwrap() as usize..self.fwr2_start.unwrap() as usize]
-                .to_vec(),
+    pub fn cdr1_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.cdr1_start,
+            self.fwr2_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn cdr1_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr1_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr1_aa(&self) -> Option<Vec<u8>> {
-        if self.cdr1_start.is_none() || self.fwr2_start.is_none() {
-            return None;
-        }
-        let start = (self.cdr1_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.fwr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn cdr1_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr1_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr2_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr2_start.is_none() || self.cdr2_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.fwr2_start.unwrap() as usize..self.cdr2_start.unwrap() as usize]
-                .to_vec(),
+    pub fn fwr2_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr2_start,
+            self.cdr2_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn fwr2_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr2_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr2_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr2_start.is_none() || self.cdr2_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr2_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr2_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn cdr2_nt(&self) -> Option<Vec<u8>> {
-        if self.cdr2_start.is_none() || self.fwr3_start.is_none() {
-            return None;
-        }
-        Some(
-            self.nt_sequence[self.cdr2_start.unwrap() as usize..self.fwr3_start.unwrap() as usize]
-                .to_vec(),
+    pub fn cdr2_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.cdr2_start,
+            self.fwr3_start,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
         )
     }
-    pub fn cdr2_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr2_nt().as_deref().unwrap().to_vec()).ok()
+    pub fn fwr3_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            self.fwr3_start,
+            Some(self.cdr3_start),
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
+        )
     }
-    pub fn cdr2_aa(&self) -> Option<Vec<u8>> {
-        if self.cdr2_start.is_none() || self.fwr3_start.is_none() {
-            return None;
-        }
-        let start = (self.cdr2_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.fwr3_start.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn cdr2_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.cdr2_aa().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr3_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr3_start.is_none() {
-            return None;
-        }
-        Some(self.nt_sequence[self.fwr3_start.unwrap() as usize..self.cdr3_start as usize].to_vec())
-    }
-    pub fn fwr3_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr3_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr3_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr3_start.is_none() {
-            return None;
-        }
-        let start = (self.fwr3_start.unwrap() as usize - self.v_start as usize) / 3;
-        let end = (self.cdr3_start - self.v_start) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr3_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr3_aa().as_deref().unwrap().to_vec()).ok()
+    pub fn fwr4_region(&self) -> Option<AnnRegion> {
+        make_ann_region(
+            Some(self.cdr3_end),
+            self.fwr4_end,
+            &self.nt_sequence,
+            self.v_start,
+            &self.aa_sequence,
+        )
     }
     pub fn cdr3_nt(&self) -> &[u8] {
         &self.nt_sequence[self.cdr3_start as usize..self.cdr3_end as usize]
@@ -291,25 +182,5 @@ impl ExactSubClonotypeChain {
     }
     pub fn cdr3_aa_string(&self) -> String {
         std::str::from_utf8(self.cdr3_aa()).unwrap().to_string()
-    }
-    pub fn fwr4_nt(&self) -> Option<Vec<u8>> {
-        if self.fwr4_end.is_none() {
-            return None;
-        }
-        Some(self.nt_sequence[self.cdr3_end as usize..self.fwr4_end.unwrap() as usize].to_vec())
-    }
-    pub fn fwr4_nt_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr4_nt().as_deref().unwrap().to_vec()).ok()
-    }
-    pub fn fwr4_aa(&self) -> Option<Vec<u8>> {
-        if self.fwr4_end.is_none() {
-            return None;
-        }
-        let start = (self.cdr3_end as usize - self.v_start as usize) / 3;
-        let end = (self.fwr4_end.unwrap() as usize - self.v_start as usize) / 3;
-        Some(self.aa_sequence[start as usize..end as usize].to_vec())
-    }
-    pub fn fwr4_aa_string(&self) -> Option<String> {
-        String::from_utf8(self.fwr4_aa().as_deref().unwrap().to_vec()).ok()
     }
 }
