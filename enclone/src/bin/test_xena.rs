06b8c619e8c86e295bde1677c017cb9e6905bfbf
David Jaffe <david.jaffe@10xgenomics.com>
2021-02-13 13:51:15 -0800

    rustfmt

--- enclone/src/bin/test_xena.rs
+++ enclone/src/bin/test_xena.rs
@@ -28,29 +28,28 @@ fn main() {
         println!("xena call for {} took {:.2} seconds", q, elapsed(&t));
     });
 
-/*
-    use std::process::Command;
-    ids.par_iter_mut().for_each(|q| {
-        let t = Instant::now();
-        let url = format!("https://xena.fuzzplex.com/api/analyses/{}", q);
-        let o = Command::new("curl")
-            .arg(url.clone())
-            .output()
-            .expect("failed to execute xena http");
-        let m = String::from_utf8(o.stdout).unwrap();
-        if o.status.code() != Some(0) {
-            let merr = String::from_utf8(o.stderr).unwrap();
-            eprintln!(
-                "\nSomething went wrong. the URL \
-                \nhttp://xena.fuzzplex.com/api/analyses/{}\n\
-                failed with the following stderr:\n{}\n\
-                and the following stdout:\n{}\n",
-                q, merr, m
-            );
-            std::process::exit(1);
-        }   
-        println!("xena call for {} took {:.2} seconds", q, elapsed(&t));
-    });
-*/
-
+    /*
+        use std::process::Command;
+        ids.par_iter_mut().for_each(|q| {
+            let t = Instant::now();
+            let url = format!("https://xena.fuzzplex.com/api/analyses/{}", q);
+            let o = Command::new("curl")
+                .arg(url.clone())
+                .output()
+                .expect("failed to execute xena http");
+            let m = String::from_utf8(o.stdout).unwrap();
+            if o.status.code() != Some(0) {
+                let merr = String::from_utf8(o.stderr).unwrap();
+                eprintln!(
+                    "\nSomething went wrong. the URL \
+                    \nhttp://xena.fuzzplex.com/api/analyses/{}\n\
+                    failed with the following stderr:\n{}\n\
+                    and the following stdout:\n{}\n",
+                    q, merr, m
+                );
+                std::process::exit(1);
+            }
+            println!("xena call for {} took {:.2} seconds", q, elapsed(&t));
+        });
+    */
 }
