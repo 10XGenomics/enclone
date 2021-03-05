7dd8421636a3143eb3029c8770cf4daf63be4c1b
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-05 12:15:06 -0800

    split a file

--- enclone_main/tests/enclone_test.rs
+++ enclone_main/tests/enclone_test.rs
@@ -2149,7 +2149,7 @@ fn test_cpu_usage() {
 #[test]
 fn test_source_code_file_length() {
     PrettyTrace::new().on();
-    const MAX_RS_LINES: usize = 2560;
+    const MAX_RS_LINES: usize = 2459;
     let top = dir_list("..");
     let mut dirs = Vec::<String>::new();
     for d in top.iter() {
