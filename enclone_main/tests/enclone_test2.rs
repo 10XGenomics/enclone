9b4ad81fc4ce28f112f559ddaec148ba9bb6318b
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-07 03:21:53 -0800

    add test for duplicated crates

--- enclone_main/tests/enclone_test2.rs
+++ enclone_main/tests/enclone_test2.rs
@@ -966,3 +966,41 @@ fn test_source_code_file_length() {
         std::process::exit(1);
     }
 }
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+// 30. Cap number of duplicated crates.
+
+// NOT BASIC
+
+#[cfg(not(feature = "basic"))]
+#[cfg(not(feature = "cpu"))]
+#[cfg(not(feature = "mem"))]
+#[test]
+fn test_dupped_crates() {
+    PrettyTrace::new().on();
+    let mut crates = Vec::<String>::new();
+    let f = open_for_read!["../Cargo.lock"];
+    let mut lines = Vec::<String>::new();
+    for line in f.lines() {
+        let s = line.unwrap();
+        lines.push(s.to_string());
+    }
+    for i in 0..lines.len() {
+        if lines[i] == "[[package]]" {
+            crates.push(lines[i + 1].clone());
+        }
+    }
+    let n1 = crates.len();
+    unique_sort(&mut crates);
+    let n2 = crates.len();
+    let d = n1 - n2;
+    const DUPPED_CRATES: usize = 22;
+    if d != DUPPED_CRATES {
+        eprintln!(
+            "\nThe number of duplicated crates is {}, but the required number is {}.\n",
+            d, DUPPED_CRATES,
+        );
+        std::process::exit(1);
+    }
+}
