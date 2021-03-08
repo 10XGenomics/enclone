ec92f3ce564cc34777841ffc84dde5eeefe67b8a
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-08 11:49:12 -0800

    make dup_crates print more

--- enclone/src/bin/dup_crates.rs
+++ enclone/src/bin/dup_crates.rs
@@ -1,11 +1,12 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
-// Count duplicated crates.
+// Find duplicated crates.
 
 use io_utils::*;
 use pretty_trace::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use string_utils::*;
 use vector_utils::*;
 
 fn main() {
@@ -22,8 +23,23 @@ fn main() {
             crates.push(lines[i + 1].clone());
         }
     }
-    let n1 = crates.len();
-    unique_sort(&mut crates);
-    let n2 = crates.len();
-    println!("{}", n1 - n2);
+    let mut dups = 0;
+    let mut duplist = Vec::<(isize, String)>::new();
+    let mut i = 0;
+    while i < crates.len() {
+        let j = next_diff(&crates, i);
+        if j - i > 1 {
+            dups += j - i - 1;
+            duplist.push((
+                i as isize - j as isize,
+                crates[i].between("\"", "\"").to_string(),
+            ));
+        }
+        i = j;
+    }
+    duplist.sort();
+    println!("{}", dups);
+    for x in duplist.iter() {
+        println!("{} {}", -x.0, x.1);
+    }
 }
