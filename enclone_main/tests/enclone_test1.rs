0e5681932196b0ebd406ee959545f890cb15dccb
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-11 15:08:27 -0800

    fix some bugs, but tests still don't pass

--- enclone_main/tests/enclone_test1.rs
+++ enclone_main/tests/enclone_test1.rs
@@ -1036,7 +1036,7 @@ fn test_for_broken_links_and_spellcheck() {
         plasmablast preinstalled prepends pwm pwms redownloads \
         researchsquare samtools screenshot segn \
         sloooooooow spacebar stackexchange standalone stdout sthnqedkr subclonotype \
-        subclonotypes svg thresholding timepoint tracebacks trb tsv twosie ubuntu \
+        subclonotypes svg testlist thresholding timepoint tracebacks trb tsv twosie ubuntu \
         umi umis underperforming unicode untarring vdj vilella vilfwym vilm website wget wikimedia \
         wikipedia workaround workflow xf xhtml xkcd xxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx zenodo zx";
     let extra_words = extra_words.split(' ').collect::<Vec<&str>>();
