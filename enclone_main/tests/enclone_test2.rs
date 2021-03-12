48242b8de99b16e848a7c6d11e02195b4391dcdd
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-12 13:21:57 -0800

    reset line ceiling

--- enclone_main/tests/enclone_test2.rs
+++ enclone_main/tests/enclone_test2.rs
@@ -906,7 +906,7 @@ fn test_cpu_usage() {
 #[test]
 fn test_source_code_file_length() {
     PrettyTrace::new().on();
-    const MAX_RS_LINES: usize = 1462; // group.rs
+    const MAX_RS_LINES: usize = 1418; // print_clonotypes.rs
     let top = dir_list("..");
     let mut dirs = Vec::<String>::new();
     for d in top.iter() {
