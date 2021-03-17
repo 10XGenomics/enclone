36846bdd73ffcd946e619808eceb77a229f7e468
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-17 02:17:26 -0700

    tidy

--- enclone/src/bin/plot_test.rs
+++ enclone/src/bin/plot_test.rs
@@ -4,16 +4,7 @@
 
 use plotters::prelude::*;
 
-/*
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut backend = SVGBackend::new("output.svg", (800, 600));
-    backend.draw_rect((50, 50), (200, 150), &RED, true)?;
-    Ok(())
-}
-*/
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let root = BitMapBackend::new("plotters-doc-data/5.png", (640, 480)).into_drawing_area();
     let root = SVGBackend::new("output.svg", (640, 480)).into_drawing_area();
     // After this point, we should be able to draw construct a chart context
     let mut chart = ChartBuilder::on(&root)
