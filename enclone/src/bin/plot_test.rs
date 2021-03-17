951275701e82ae2859352d8a89fb2385ebbf3a8f
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-17 02:48:16 -0700

    plot test work in progress

--- enclone/src/bin/plot_test.rs
+++ enclone/src/bin/plot_test.rs
@@ -4,17 +4,23 @@
 
 use plotters::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = SVGBackend::new("output.svg", (640, 480)).into_drawing_area();
+fn main() {
+
+    let points = vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)];
+    let title = "This is our first plot";
+    let plotfile = "/mnt/home/david.jaffe/public_html/plotz.svg";
+
+    let root = SVGBackend::new(&plotfile, (800, 600)).into_drawing_area();
+    let root = root.margin(10, 10, 10, 10);
     // After this point, we should be able to draw construct a chart context
     let mut chart = ChartBuilder::on(&root)
         // Set the caption of the chart
-        .caption("This is our first plot", ("sans-serif", 40).into_font())
+        .caption(&title, ("sans-serif", 40).into_font())
         // Set the size of the label region
         .x_label_area_size(20)
         .y_label_area_size(40)
         // Finally attach a coordinate on the drawing area and make a chart context
-        .build_cartesian_2d(0f32..10f32, 0f32..10f32)?;
+        .build_cartesian_2d(0f32..10f32, 0f32..10f32).unwrap();
 
     // Then we can draw a mesh
     chart
@@ -24,16 +30,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .y_labels(5)
         // We can also change the format of the label text
         .y_label_formatter(&|x| format!("{:.3}", x))
-        .draw()?;
+        .draw().unwrap();
 
     // And we can draw something in the drawing area
     chart.draw_series(LineSeries::new(
-        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        points.clone(),
         &RED,
-    ))?;
+    )).unwrap();
     // Similarly, we can draw point series
     chart.draw_series(PointSeries::of_element(
-        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        points.clone(),
         5,
         &RED,
         &|c, s, st| {
@@ -41,6 +47,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
             + Text::new(format!("{:?}", c), (10, 0), ("sans-serif", 10).into_font());
         },
-    ))?;
-    Ok(())
+    )).unwrap();
 }
