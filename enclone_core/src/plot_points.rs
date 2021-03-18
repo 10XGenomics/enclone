c4562f0a5041debaae021db3b7436a4f997ca6b1
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-18 06:22:37 -0700

    several interim tweaks to appearance

--- enclone_core/src/plot_points.rs
+++ enclone_core/src/plot_points.rs
@@ -14,7 +14,7 @@ pub fn plot_points(points: &Vec<(f32, f32)>, xvar: &str, yvar: &str, svg_filenam
     // Possibly universal constants.
 
     let title_font_size = 30;
-    let font = "sans-serif";
+    let font = "arial";
     let tic_font_size = 20;
     let axis_tics = 5;
     let point_size = 4;
@@ -26,10 +26,8 @@ pub fn plot_points(points: &Vec<(f32, f32)>, xvar: &str, yvar: &str, svg_filenam
 
     // Determine the plot ranges using the extreme values of the points, extended a little bit.
 
-    let mut xlow = points[0].0;
-    let mut xhigh = points[0].0;
-    let mut ylow = points[0].1;
-    let mut yhigh = points[0].1;
+    let (mut xlow, mut xhigh) = (points[0].0, points[0].0);
+    let (mut ylow, mut yhigh) = (points[0].1, points[0].1);
     for i in 1..points.len() {
         xlow = xlow.min(points[i].0);
         xhigh = xhigh.max(points[i].0);
@@ -47,23 +45,29 @@ pub fn plot_points(points: &Vec<(f32, f32)>, xvar: &str, yvar: &str, svg_filenam
     } else {
         ylow *= 1.0 - range_ext;
     }
-    yhigh *= 1.0 + range_ext;
-
-    // Determine the precision for the x axis tics.
-
-    let mut x_precision = 0;
-    let m = xhigh.abs();
-    if m > 0.0 {
-        let extra = 2;
-        x_precision = -m.log10() as usize + extra;
+    if yhigh > 0.0 {
+        yhigh *= 1.0 + range_ext;
+    } else {
+        yhigh *= 1.0 - range_ext;
     }
 
-    // Determine the precision for the y axis tics.
+    // Determine precision for axes tics.
 
-    let mut y_precision = 0;
+    let (mut x_precision, mut y_precision) = (0, 0);
+    let m = xhigh.abs();
+    if m > 0.0 {
+        let mut extra = 2;
+        if m > 10.0 {
+            extra = 0;
+        }
+        x_precision = -m.log10() as usize + extra;
+    }
     let m = yhigh.abs();
     if m > 0.0 {
-        let extra = 2;
+        let mut extra = 1;
+        if m > 10.0 {
+            extra = 0;
+        }
         y_precision = -m.log10() as usize + extra;
     }
 
@@ -77,11 +81,14 @@ pub fn plot_points(points: &Vec<(f32, f32)>, xvar: &str, yvar: &str, svg_filenam
     if yhigh.abs() > 1.0 {
         y_digits_to_left = yhigh.abs().log10().floor() as usize + 1
     }
-    let y_len = y_digits_to_left + y_precision;
+    let mut y_len = y_digits_to_left + y_precision;
+    if ylow < 0.0 {
+        y_len += 1;
+    }
     let y_label_area_size = y_len as f64 * (tic_font_size as f64 * 0.6)
         + tic_font_size as f64 * 0.2
         + tic_font_size as f64;
-    let y_label_area_size = y_label_area_size as u32;
+    let y_label_area_size = y_label_area_size as u32 + 5;
 
     // Make the plot.
 
