be0f1967e33dd5920b314528f8a340bfc364420b
David Jaffe <david.jaffe@10xgenomics.com>
2021-03-28 08:03:15 -0700

    add to dictionary

--- enclone_main/tests/enclone_test2.rs
+++ enclone_main/tests/enclone_test2.rs
@@ -311,7 +311,7 @@ fn test_for_broken_links_and_spellcheck() {
 
     let extra_words =
         "abybank adefghiklmnpqrstvwy amazonaws anarci barcode barcodes barcoding bcn \
-        bioinf cdiff cellranger chmod clono clonotype clonotypes \
+        bioinf cdiff cellranger chmod clen clono clonotype clonotypes \
         clonotyping codebase colorn contig contigs cqvwdsssdhpyvf cred crispr \
         csv ctrlc cvar cvars datalayer dejavusansmono dotplot \
         dref dyiid enclone executables false fcell \
@@ -326,8 +326,9 @@ fn test_for_broken_links_and_spellcheck() {
         plasmablast preinstalled prepends pwm pwms redownloads \
         researchsquare samtools screenshot segn \
         sloooooooow spacebar stackexchange standalone stdout sthnqedkr subclonotype \
-        subclonotypes svg testlist thresholding timepoint tracebacks trb tsv twosie ubuntu \
-        umi umis underperforming unicode untarring vdj vilella vilfwym vilm website wget wikimedia \
+        subclonotypes svg testlist thresholding timepoint tracebacks trb tsv twosie ubuntu udiff \
+        umi umis underperforming unicode untarring \
+        vdj vilella vilfwym vilm vjlen website wget wikimedia \
         wikipedia workaround workflow xf xhtml xkcd \
         xxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xy yvar zenodo zx";
     let extra_words = extra_words.split(' ').collect::<Vec<&str>>();
