5a40514066f5c61a2476712951b669adc379bc54
David Jaffe <david.jaffe@10xgenomics.com>
2021-04-05 04:25:23 -0700

    add test for garbage variable in linear constraint

--- enclone_core/src/testlist.rs
+++ enclone_core/src/testlist.rs
@@ -24,7 +24,7 @@ pub fn enclone_testdata_public_gex_human() -> String {
 
 pub const TEST_FILES_VERSION: u8 = 15;
 
-pub const TESTS: [&str; 231] = [
+pub const TESTS: [&str; 232] = [
     // 1. tests variant base after CDR3, parseable output
     r###"BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout
      PCOLS=exact_subclonotype_id,n,v_name1,v_name2,nchains,var_indices_aa1,barcodes"###,
@@ -599,6 +599,8 @@ pub const TESTS: [&str; 231] = [
     r###"BCR=123085 KEEP_CLONO_IF_CELL_MAX="nchains > 2" CDR3=CTRDRDLRGATDAFDIW"###,
     // 231. test ≤
     r###"BCR=86237 KEEP_CLONO_IF_CELL_MEAN="u2≤150" NOPRINT SUMMARY"###,
+    // 232. test nonsense variable in KEEP_CLONO_IF_CELL_MAX
+    r###"BCR=86237 KEEP_CLONO_IF_CELL_MAX="gexzz > 8000" EXPECT_FAIL"###,
 ];
 
 // Crash tests.  These are tests to make sure that certain options do not result in a crash, even
