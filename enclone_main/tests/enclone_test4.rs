69002d9aae1c3961e93047ceaa5d187e51d45a3c
David Jaffe <david.jaffe@10xgenomics.com>
2021-04-09 11:33:09 -0700

    reduce executable size req by 2.6%; ./test now passes

--- enclone_main/tests/enclone_test4.rs
+++ enclone_main/tests/enclone_test4.rs
@@ -25,7 +25,7 @@ const LOUPE_OUT_FILENAME: &str = "testx/__test_proto";
 #[test]
 fn test_executable_size() {
     PrettyTrace::new().on();
-    const ENCLONE_SIZE: usize = 79711440;
+    const ENCLONE_SIZE: usize = 77648128;
     const ENCLONE_SIZE_MAX_PER_DIFF: f64 = 1.0;
     let f = format!("../target/debug/enclone");
     let n = metadata(&f).unwrap().len() as usize;
