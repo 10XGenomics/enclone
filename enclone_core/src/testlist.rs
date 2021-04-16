de8039d0903defc427facc26f4b78fc835328b8b
David Jaffe <david.jaffe@10xgenomics.com>
2021-04-16 09:07:19 -0700

    add test

--- enclone_core/src/testlist.rs
+++ enclone_core/src/testlist.rs
@@ -24,7 +24,7 @@ pub fn enclone_testdata_public_gex_human() -> String {
 
 pub const TEST_FILES_VERSION: u8 = 15;
 
-pub const TESTS: [&str; 235] = [
+pub const TESTS: [&str; 236] = [
     // 1. tests variant base after CDR3, parseable output
     r###"BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout
      PCOLS=exact_subclonotype_id,n,v_name1,v_name2,nchains,var_indices_aa1,barcodes"###,
@@ -608,6 +608,8 @@ pub const TESTS: [&str; 235] = [
     r###"BCR=123085 CVARS=opt_d,opt_d2,opt_dΔ,opt_d_delta AMINO=cdr3 CDR3=CARVRDILTGDYGMDVW"###,
     // 235. test ALIGN<n>
     r###"BCR=123085 CDR3=CKVMLYDSRGSDYYYVMDVW ALIGN1 CVARS=opt_d"###,
+    // 236. test ALIGN<n> and JUN_ALIGN<n>, case where there's a D segment
+    r###"BCR=85333 ALIGN1 JUN_ALIGN1 CDR3=CARGYDFWSGYLVGNWAGDYYYYMDVW"###,
 ];
 
 // Crash tests.  These are tests to make sure that certain options do not result in a crash, even
