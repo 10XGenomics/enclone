180232917754a20f15e32b4e7a53190399e58dcd
David Jaffe <david.jaffe@10xgenomics.com>
2021-04-21 04:58:33 -0700

    delete more stuff we're not using

--- bio_edit/src/utils/mod.rs
+++ bio_edit/src/utils/mod.rs
@@ -6,10 +6,6 @@
 mod text;
 pub use self::text::{trim_newline, Text, TextSlice};
 
-mod interval;
-pub use self::interval::Interval;
-
-/// In place implementation of scan over a slice.
 pub fn scan<T: Copy, F: Fn(T, T) -> T>(a: &mut [T], op: F) {
     let mut s = a[0];
     for v in a.iter_mut().skip(1) {
@@ -18,7 +14,6 @@ pub fn scan<T: Copy, F: Fn(T, T) -> T>(a: &mut [T], op: F) {
     }
 }
 
-// Inplace implementation of prescan over a slice.
 pub fn prescan<T: Copy, F: Fn(T, T) -> T>(a: &mut [T], neutral: T, op: F) {
     let mut s = neutral;
     for v in a.iter_mut() {
