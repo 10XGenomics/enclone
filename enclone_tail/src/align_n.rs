966130aaaee7ee8fa09fcf7bb7bdb63abc6c45d8
DJ <david.jaffe@10xgenomics.com>
2021-05-03 14:46:43 -0700

    Update enclone_tail/src/align_n.rs
    Co-authored-by: wyattmcdonnell <18267139+wyattmcdonnell@users.noreply.github.com>

--- enclone_tail/src/align_n.rs
+++ enclone_tail/src/align_n.rs
@@ -1,6 +1,6 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 //
-// Parallized precompute for ALIGN<n> and JALIGN<n>.
+// Parallelized precompute for ALIGN<n> and JALIGN<n>.
 
 use align_tools::*;
 use amino::*;
