f16c61c87d207f535aff2fd42011796fe886b33b
David Jaffe <david.jaffe@10xgenomics.com>
2021-05-06 14:29:11 -0700

    add test

--- enclone_core/src/testlist.rs
+++ enclone_core/src/testlist.rs
@@ -71,7 +71,7 @@ pub const DTESTS: [&str; 15] = [
 // Tests that are affected by the grouping algorithm.  All such tests should go here, if not
 // already in DTESTS.
 
-pub const GTESTS: [&str; 10] = [
+pub const GTESTS: [&str; 11] = [
     // 1. test 5/8 for newline correctness (this grouping option deprecated but supported)
     r###"BCR=85333 GROUP_VJ_REFNAME MIN_GROUP=2 AMINO= PLAIN SET_IN_STONE"###,
     // 2. test 6/8 for newline correctness (this grouping option deprecated but supported)
@@ -98,6 +98,9 @@ pub const GTESTS: [&str; 10] = [
     // 10. test of AGROUP
     r###"BCR=123085 AGROUP AG_CENTER=from_filters CDR3=CARHSYSSGWYDEWDYW
          AG_DIST_FORMULA=cdr3_edit_distance AG_DIST_BOUND=top=2"###,
+    // 11. test of AGROUP
+    r###"BCR=123085 AGROUP AG_CENTER=from_filters CDR3=CAKDGGEHYYDSSGYYASYYFDYW 
+         AG_DIST_FORMULA=cdr3_edit_distance AG_DIST_BOUND=max=14"###,
 ];
 
 pub const TESTS: [&str; 234] = [
