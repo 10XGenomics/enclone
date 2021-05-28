05538924efb227de85d861907d36e66fce9a5f61
David Jaffe <david.jaffe@10xgenomics.com>
2021-05-28 14:57:27 -0700

    nothing change

--- enclone_main/src/enclone_client.rs
+++ enclone_main/src/enclone_client.rs
@@ -52,7 +52,6 @@
 //
 // 2.  Can carriage return be used instead of pushing a button?
 
-
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 // This is a GUI client, work in progress.
