aeaa7937fecb2cb3c670dfcfecbfb7891f8483a3
David Jaffe <david.jaffe@10xgenomics.com>
2021-06-01 18:44:23 -0700

    fix bug in get_config

--- enclone_core/src/defs.rs
+++ enclone_core/src/defs.rs
@@ -961,8 +961,8 @@ pub struct PotentialJoin {
 pub fn get_config(config_file: &str, config: &mut HashMap<String, String>) -> bool {
     if config_file.len() > 0 {
         let mut cf = config_file.to_string();
-        if cf.contains(',') {
-            cf = cf.after(",").to_string();
+        if cf.contains(':') {
+            cf = cf.after(":").to_string();
         }
         if path_exists(&cf) {
             let f = open_for_read![&cf];
