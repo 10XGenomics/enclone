507f9d0019d3f554aa5b34d9f6ae69c40c25c23a
David Jaffe <david.jaffe@10xgenomics.com>
2021-06-02 08:54:03 -0700

    now shows button and a circle, and compiles

--- enclone_visual/src/bin/circ.rs
+++ enclone_visual/src/bin/circ.rs
@@ -1,7 +1,9 @@
 use iced::{
-    canvas::{self, Canvas, Cursor, Frame, Geometry, Path},
-    button, Button, Color, Container, Element, Length, Rectangle, Sandbox, Settings, Text,
+    // canvas::{self, Canvas, Cursor, Frame, Geometry, Path},
+    button, Button, Column, Container, Element, Length, Sandbox, Settings, Text,
 };
+
+use grid::Grid;
 
 pub fn main() -> iced::Result {
     Clock::run(Settings::default())
@@ -9,6 +11,7 @@ pub fn main() -> iced::Result {
 
 #[derive(Default)]
 struct Clock {
+    grid: Grid,
     button: button::State,
 }
 
@@ -44,6 +47,7 @@ impl Sandbox for Clock {
             .padding(10)
             .on_press(Message::ButtonPressed);
 
+        /*
         let canvas = Canvas::new(self)
             .width(Length::Units(400))
             .height(Length::Units(400));
@@ -55,9 +59,97 @@ impl Sandbox for Clock {
             .center_x()
             .center_y()
             .into()
+        */
+
+        let content = Column::new()
+            .push(button)
+            .push(
+                self.grid
+                    .view()
+                    .map(move |message| Message::ButtonPressed),
+            );
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            // .style(style::Container)
+            .into()
+
     }
 }
 
+mod grid {
+    use iced::{
+        // canvas::event::{self, Event},
+        canvas::{self, Canvas, Cursor, Frame, Geometry, Path},
+        Color, Element, Length, Rectangle,
+    };
+
+    #[derive(Default)]
+    struct State {
+    }
+
+    pub struct Grid {
+        state: State,
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Message {
+    }
+
+    impl Default for Grid {
+        fn default() -> Self {
+            Self { 
+                state: State::default(),
+            }
+            // Self::from_preset(Preset::default())
+        }
+    }
+
+    impl Grid {
+
+        pub fn update(&mut self, message: Message) {
+            match message {
+            }
+        }
+
+        pub fn view<'a>(&'a mut self) -> Element<'a, Message> {
+            Canvas::new(self)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .into()
+        }
+
+        /*
+        fn visible_region(&self, size: Size) -> Region {
+            let width = size.width / self.scaling;
+            let height = size.height / self.scaling;
+
+            Region {
+                x: -self.translation.x - width / 2.0,
+                y: -self.translation.y - height / 2.0,
+                width,
+                height,
+            }
+        }
+        */
+    }
+
+    impl<'a> canvas::Program<Message> for Grid {
+        fn draw(&self, bounds: Rectangle, _cursor: Cursor) -> Vec<Geometry> {
+            let mut frame = Frame::new(bounds.size());
+            let radius = 100.0;
+            let circle = Path::circle(frame.center(), radius);
+            frame.fill(&circle, Color::BLACK);
+            vec![frame.into_geometry()]
+        }
+    }
+}
+
+
+
+
+/*
 impl canvas::Program<Message> for Clock {
     fn draw(&self, bounds: Rectangle, _cursor: Cursor) -> Vec<Geometry> {
         let mut frame = Frame::new(bounds.size());
@@ -67,3 +159,4 @@ impl canvas::Program<Message> for Clock {
         vec![frame.into_geometry()]
     }
 }
+*/
