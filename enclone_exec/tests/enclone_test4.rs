3b9bacdba59ab33ff15e669e7e7c55b4658e6c6a
Shaun Jackman <sjackman@gmail.com>
2021-06-03 13:50:44 -0700

    Compress debug sections on Linux (#253)
    this dramatically reduces the Linux executable size

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -25,7 +25,7 @@ const LOUPE_OUT_FILENAME: &str = "testx/__test_proto";
 #[test]
 fn test_executable_size() {
     PrettyTrace::new().on();
-    const ENCLONE_SIZE: usize = 234120464;
+    const ENCLONE_SIZE: usize = 80518696;
     const ENCLONE_SIZE_MAX_PER_DIFF: f64 = 1.0;
     let f = format!("../target/debug/enclone");
     let n = metadata(&f).unwrap().len() as usize;
