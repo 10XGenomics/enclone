2b008b8d3e59f33f81d30b2c29518ab47802c08f
David Jaffe <david.jaffe@10xgenomics.com>
2021-06-15 06:49:47 -0700

    fix bug; ./test passes

--- enclone_tail/src/plot.rs
+++ enclone_tail/src/plot.rs
@@ -912,7 +912,9 @@ pub fn plot_clonotypes(
 
     // Output the svg file.
 
-    if plot_opt.plot_file != "stdout" && plot_opt.plot_file != "gui" && plot_opt.plot_file != "gui"
+    if plot_opt.plot_file != "stdout"
+        && plot_opt.plot_file != "gui"
+        && plot_opt.plot_file != "gui_stdout"
     {
         let f = File::create(&plot_opt.plot_file);
         if f.is_err() {
