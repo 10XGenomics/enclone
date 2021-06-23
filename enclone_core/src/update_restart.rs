833d2047af4b28b44487cc4725108b4232f25282
David Jaffe <david.jaffe@10xgenomics.com>
2021-06-23 09:46:04 -0700

    refactor

--- enclone_core/src/update_restart.rs
+++ enclone_core/src/update_restart.rs
@@ -2,6 +2,7 @@
 
 use std::env;
 use std::process::Command;
+use string_utils::*;
 
 pub fn restart_enclone() {
     let args: Vec<String> = env::args().collect();
@@ -15,4 +16,36 @@ pub fn restart_enclone() {
         .expect("failed to execute enclone restart");
     let _ = o.wait().expect("failed to wait on child");
     std::process::exit(0);
+}
+
+pub fn update_enclone() {
+    println!("Automatically updating enclone, following the instructions at bit.ly/enclone.\n");
+    let mut home = String::new();
+    for (key, value) in env::vars() {
+        if key == "HOME" {
+            home = value.clone();
+        }
+    }
+    if home.len() == 0 {
+        eprintln!("Weird, unable to determine your home directory.\n");
+        std::process::exit(1);
+    }
+    let o = Command::new("curl")
+        .arg("-s")
+        .arg("-L")
+        .arg(
+            "https://github.com/10XGenomics/enclone/\
+            releases/latest/download/enclone_macos",
+        )
+        .arg("--output")
+        .arg(&format!("{}/bin/enclone", home))
+        .output()
+        .expect("failed to execute curl");
+    if o.status.code() != Some(0) {
+        eprintln!(
+            "Update failed with the following error message:\n{}",
+            strme(&o.stderr)
+        );
+        std::process::exit(1);
+    }
 }
--- enclone_visual/src/enclone_client.rs
+++ enclone_visual/src/enclone_client.rs
@@ -477,38 +477,7 @@ pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error
                 eprintln!("local enclone version = {}", local_version);
                 eprintln!("\nYour enclone version is not up to date.");
                 if auto_update {
-                    println!(
-                        "Automatically updating enclone, following the instructions at \
-                        bit.ly/enclone.\n"
-                    );
-                    let mut home = String::new();
-                    for (key, value) in env::vars() {
-                        if key == "HOME" {
-                            home = value.clone();
-                        }
-                    }
-                    if home.len() == 0 {
-                        eprintln!("Weird, unable to determine your home directory.\n");
-                        std::process::exit(1);
-                    }
-                    let o = Command::new("curl")
-                        .arg("-s")
-                        .arg("-L")
-                        .arg(
-                            "https://github.com/10XGenomics/enclone/\
-                            releases/latest/download/enclone_macos",
-                        )
-                        .arg("--output")
-                        .arg(&format!("{}/bin/enclone", home))
-                        .output()
-                        .expect("failed to execute curl");
-                    if o.status.code() != Some(0) {
-                        eprintln!(
-                            "Update failed with the following error message:\n{}",
-                            strme(&o.stderr)
-                        );
-                        std::process::exit(1);
-                    }
+                    update_enclone();
                     println!("Done, restarting!\n");
                     restart_enclone();
                 } else {
