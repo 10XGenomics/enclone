b17059a03fef4a40f620134bfd85573ba336052f
David Jaffe <david.jaffe@10xgenomics.com>
2021-06-23 07:05:30 -0700

    refactor

--- enclone_visual/src/enclone_client.rs
+++ enclone_visual/src/enclone_client.rs
@@ -51,6 +51,20 @@ use string_utils::*;
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
+fn restart() {
+    let args: Vec<String> = env::args().collect();
+    let mut args1 = Vec::<String>::new();
+    for i in 1..args.len() {
+        args1.push(args[i].clone());
+    }
+    let mut o = Command::new("enclone")
+        .args(&args1)
+        .spawn()
+        .expect("failed to execute enclone restart");
+    let _ = o.wait().expect("failed to wait on child");
+    std::process::exit(0);
+}
+
 pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error>> {
     //
     // Fail if not running on a Mac.
@@ -411,17 +425,7 @@ pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error
             thread::sleep(Duration::from_millis(5000));
             if !READ_DONE.load(SeqCst) {
                 println!("darn, we seem to have hit a bad port, so restarting");
-                let args: Vec<String> = env::args().collect();
-                let mut args1 = Vec::<String>::new();
-                for i in 1..args.len() {
-                    args1.push(args[i].clone());
-                }
-                let mut o = Command::new("enclone")
-                    .args(&args1)
-                    .spawn()
-                    .expect("failed to execute enclone restart");
-                let _ = o.wait().expect("failed to wait on child");
-                std::process::exit(0);
+                restart();
             }
         });
         server_stdout.read(&mut buffer).unwrap();
