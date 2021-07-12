35497c50507ec4c55c326a74450d47d3f85d1ec2
David Jaffe <david.jaffe@10xgenomics.com>
2021-07-12 13:19:40 -0700

    update crates; one less duplicated crates; compiles, not tested

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -182,7 +182,7 @@ fn test_dupped_crates() {
     unique_sort(&mut crates);
     let n2 = crates.len();
     let d = n1 - n2;
-    const DUPPED_CRATES: usize = 51;
+    const DUPPED_CRATES: usize = 50;
     if d != DUPPED_CRATES {
         eprintln!(
             "\nThe number of duplicated crates is {}, but the required number is {}.\n",
