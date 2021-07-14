df2baaec12f526cba29ca759abd51d3eae83d499
David Jaffe <david.jaffe@10xgenomics.com>
2021-07-14 10:46:39 -0700

    slightly reduce size of command text box

--- enclone_visual/src/gui.rs
+++ enclone_visual/src/gui.rs
@@ -138,7 +138,7 @@ impl Application for EncloneVisual {
 
             // Add command box.
 
-            const MAX_LINE: usize = 45;
+            const MAX_LINE: usize = 43;
             let mut log = String::new();
             if self.history_index >= 1 {
                 let cmd = self.translated_input_hist_uniq
