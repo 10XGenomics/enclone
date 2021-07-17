7f73e7e9acd8f94ef381f34975f343069ccfc7a5
David Jaffe <david.jaffe@10xgenomics.com>
2021-07-17 12:58:59 -0700

    loop until nothing changed

--- enclone_tools/src/bin/cargo_update_safe.rs
+++ enclone_tools/src/bin/cargo_update_safe.rs
@@ -2,8 +2,6 @@
 
 // For each dependent crate, cargo update it and run cargo b.  If that succeeds,
 // commit the change.  Otherwise, undo.
-//
-// Note that this could in principle be updated.
 
 use io_utils::*;
 use pretty_trace::*;
@@ -40,89 +38,99 @@ fn main() {
     }
     unique_sort(&mut crates);
     let mut dots = 0;
-    println!("");
-    for cratex in crates.iter() {
-        let cratex = &*cratex;
 
-        // Update crate.
+    // Loop until nothing changes.
 
-        let new = Command::new("cargo")
-            .arg("update")
-            .arg("-p")
-            .arg(&cratex)
-            .output()
-            .expect(&format!("failed to execute cargo update"));
-        if new.status.code() != Some(0) {
-            print_dot(&mut dots);
-            continue;
-        }
-
-        // See what changed in Cargo.lock.  In order to declare a change, we require that the
-        // crate in question changed.  Other things can change, and that's flaky, and if it
-        // happens, we don't do anything.
-
-        let new = Command::new("git")
-            .arg("diff")
-            .output()
-            .expect(&format!("failed to execute git diff"));
-        if new.status.code() != Some(0) {
-            println!("git diff failed, something is wrong");
-            std::process::exit(1);
-        }
-        let updated = strme(&new.stdout).contains(&format!("+ \"{} ", cratex));
-        if !updated {
-            let new = Command::new("git")
-                .arg("checkout")
-                .arg("Cargo.lock")
-                .output()
-                .expect(&format!("failed to execute git checkout"));
-            if new.status.code() != Some(0) {
-                println!("git checkout failed, something is wrong");
-                std::process::exit(1);
-            }
-            print_dot(&mut dots);
-            continue;
-        }
-
-        // Now try to compile.
-
-        let new = Command::new("cargo")
-            .arg("b")
-            .output()
-            .expect(&format!("failed to execute cargo b"));
-        if new.status.code() != Some(0) {
-            let new = Command::new("git")
-                .arg("checkout")
-                .arg("Cargo.lock")
-                .output()
-                .expect(&format!("failed to execute git checkout"));
-            if new.status.code() != Some(0) {
-                println!("git checkout failed, something is wrong");
-                println!("err =\n{}", strme(&new.stderr));
-                std::process::exit(1);
-            }
-            print_dot(&mut dots);
-            continue;
-        }
-
-        // Finally, commit the change.
-
-        dots = 0;
-        println!("\ncommitting change to {}", cratex);
-        let new = Command::new("git")
-            .arg("commit")
-            .arg("-a")
-            .arg("-m")
-            .arg(&format!("update crate {}", cratex))
-            .output()
-            .expect(&format!("failed to execute git commit"));
-        if new.status.code() != Some(0) {
-            println!("git commit failed, something is wrong");
-            std::process::exit(1);
-        }
-    }
-    if dots > 0 {
+    loop {
         println!("");
+        let mut changed = false;
+        for cratex in crates.iter() {
+            let cratex = &*cratex;
+
+            // Update crate.
+
+            let new = Command::new("cargo")
+                .arg("update")
+                .arg("-p")
+                .arg(&cratex)
+                .output()
+                .expect(&format!("failed to execute cargo update"));
+            if new.status.code() != Some(0) {
+                print_dot(&mut dots);
+                continue;
+            }
+
+            // See what changed in Cargo.lock.  In order to declare a change, we require that the
+            // crate in question changed.  Other things can change, and that's flaky, and if it
+            // happens, we don't do anything.
+
+            let new = Command::new("git")
+                .arg("diff")
+                .output()
+                .expect(&format!("failed to execute git diff"));
+            if new.status.code() != Some(0) {
+                println!("git diff failed, something is wrong");
+                std::process::exit(1);
+            }
+            let updated = strme(&new.stdout).contains(&format!("+ \"{} ", cratex));
+            if !updated {
+                let new = Command::new("git")
+                    .arg("checkout")
+                    .arg("Cargo.lock")
+                    .output()
+                    .expect(&format!("failed to execute git checkout"));
+                if new.status.code() != Some(0) {
+                    println!("git checkout failed, something is wrong");
+                    std::process::exit(1);
+                }
+                print_dot(&mut dots);
+                continue;
+            }
+
+            // Now try to compile.
+
+            let new = Command::new("cargo")
+                .arg("b")
+                .output()
+                .expect(&format!("failed to execute cargo b"));
+            if new.status.code() != Some(0) {
+                let new = Command::new("git")
+                    .arg("checkout")
+                    .arg("Cargo.lock")
+                    .output()
+                    .expect(&format!("failed to execute git checkout"));
+                if new.status.code() != Some(0) {
+                    println!("git checkout failed, something is wrong");
+                    println!("err =\n{}", strme(&new.stderr));
+                    std::process::exit(1);
+                }
+                print_dot(&mut dots);
+                continue;
+            }
+
+            // Finally, commit the change.
+
+            dots = 0;
+            println!("\ncommitting change to {}", cratex);
+            changed = true;
+            let new = Command::new("git")
+                .arg("commit")
+                .arg("-a")
+                .arg("-m")
+                .arg(&format!("update crate {}", cratex))
+                .output()
+                .expect(&format!("failed to execute git commit"));
+            if new.status.code() != Some(0) {
+                println!("git commit failed, something is wrong");
+                std::process::exit(1);
+            }
+        }
+        if dots > 0 {
+            println!("");
+        }
+        println!("");
+        if !changed {
+            break;
+        }
     }
-    println!("");
 }
