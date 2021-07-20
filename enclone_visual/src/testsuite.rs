662391cf015891359041b69f45acad208dbd746b
David Jaffe <david.jaffe@10xgenomics.com>
2021-07-20 08:49:55 -0700

    add to manual tests

--- enclone_visual/src/testsuite.rs
+++ enclone_visual/src/testsuite.rs
@@ -96,3 +96,5 @@ pub const TESTS: [(&str, MsgFn, &str); 28] = [
 // 6. test that group clicks work
 //
 // 7. test that horizontal resizing works on enclone BCR=123085 CHAINS=4
+//
+// 8. test that enclone VIS does not begin with a spinning rainbow wheel
