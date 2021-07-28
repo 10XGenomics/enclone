5db5c33aa94f734669d030af657b1074cc63dc5c
David Jaffe <david.jaffe@10xgenomics.com>
2021-07-28 05:20:20 -0700

    reorg file for readability

--- enclone_visual/src/history.rs
+++ enclone_visual/src/history.rs
@@ -1,5 +1,8 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
+// Storage of enclone visual history, and functions to save and restore.  These have not been
+// optimized.
+
 use crate::*;
 use serde::{Deserialize, Serialize};
 
@@ -34,7 +37,7 @@ pub struct EncloneVisualHistory {
     pub history_index: u32,
 }
 
-const ENCLONE_VISUAL_HISTORY_VERSION: usize = 1;
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 pub fn u32_bytes(x: usize) -> Vec<u8> {
     (x as u32).to_le_bytes().to_vec()
@@ -44,7 +47,7 @@ pub fn u32_from_bytes(x: &[u8]) -> u32 {
     u32::from_le_bytes([x[0], x[1], x[2], x[3]])
 }
 
-// Save and restore functions.  These are definitely not optimally efficient.
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 pub fn save_vec_string(x: &Vec<String>) -> Vec<u8> {
     let mut bytes = Vec::<u8>::new();
@@ -54,76 +57,6 @@ pub fn save_vec_string(x: &Vec<String>) -> Vec<u8> {
         bytes.append(&mut x[i].as_bytes().to_vec());
     }
     bytes
-}
-
-pub fn save_vec_vec_u8(x: &Vec<Vec<u8>>) -> Vec<u8> {
-    let mut bytes = Vec::<u8>::new();
-    bytes.append(&mut u32_bytes(x.len()));
-    for i in 0..x.len() {
-        bytes.append(&mut u32_bytes(x[i].len()));
-        bytes.append(&mut x[i].clone());
-    }
-    bytes
-}
-
-pub fn save_vec_vec_u32(x: &Vec<Vec<u32>>) -> Vec<u8> {
-    let mut bytes = Vec::<u8>::new();
-    bytes.append(&mut u32_bytes(x.len()));
-    for i in 0..x.len() {
-        bytes.append(&mut u32_bytes(x[i].len()));
-        for j in 0..x[i].len() {
-            bytes.append(&mut x[i][j].to_le_bytes().to_vec());
-        }
-    }
-    bytes
-}
-
-pub fn save_vec_bool(x: &Vec<bool>) -> Vec<u8> {
-    let mut bytes = Vec::<u8>::new();
-    bytes.append(&mut u32_bytes(x.len()));
-    for i in 0..x.len() {
-        bytes.push(if x[i] { 1 } else { 0 });
-    }
-    bytes
-}
-
-pub fn save_vec_u32(x: &Vec<u32>) -> Vec<u8> {
-    let mut bytes = Vec::<u8>::new();
-    bytes.append(&mut u32_bytes(x.len()));
-    for i in 0..x.len() {
-        bytes.append(&mut x[i].to_le_bytes().to_vec());
-    }
-    bytes
-}
-
-pub fn save_u32(x: u32) -> Vec<u8> {
-    x.to_le_bytes().to_vec()
-}
-
-pub fn restore_u32(x: &Vec<u8>, pos: &mut usize) -> Result<u32, ()> {
-    if *pos + 4 > x.len() {
-        return Err(());
-    }
-    let y = u32_from_bytes(&x[*pos..*pos + 4]);
-    *pos += 4;
-    Ok(y)
-}
-
-pub fn restore_vec_u32(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<u32>, ()> {
-    if *pos + 4 > x.len() {
-        return Err(());
-    }
-    let n = u32_from_bytes(&x[*pos..*pos + 4]) as usize;
-    *pos += 4;
-    if *pos + 4 * n > x.len() {
-        return Err(());
-    }
-    let mut y = vec![0; n];
-    for j in 0..n {
-        y[j] = u32_from_bytes(&x[*pos..*pos + 4]);
-        *pos += 4;
-    }
-    Ok(y)
 }
 
 pub fn restore_vec_string(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<String>, ()> {
@@ -152,21 +85,16 @@ pub fn restore_vec_string(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<String>, (
     Ok(y)
 }
 
-pub fn restore_vec_bool(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<bool>, ()> {
-    if *pos + 4 > x.len() {
-        return Err(());
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+pub fn save_vec_vec_u8(x: &Vec<Vec<u8>>) -> Vec<u8> {
+    let mut bytes = Vec::<u8>::new();
+    bytes.append(&mut u32_bytes(x.len()));
+    for i in 0..x.len() {
+        bytes.append(&mut u32_bytes(x[i].len()));
+        bytes.append(&mut x[i].clone());
     }
-    let n = u32_from_bytes(&x[*pos..*pos + 4]) as usize;
-    *pos += 4;
-    if *pos + n > x.len() {
-        return Err(());
-    }
-    let mut y = vec![false; n];
-    for j in 0..n {
-        y[j] = if *pos == 1 { true } else { false };
-        *pos += 1;
-    }
-    Ok(y)
+    bytes
 }
 
 pub fn restore_vec_vec_u8(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<Vec<u8>>, ()> {
@@ -189,6 +117,20 @@ pub fn restore_vec_vec_u8(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<Vec<u8>>, 
         *pos += k;
     }
     Ok(y)
+}
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+pub fn save_vec_vec_u32(x: &Vec<Vec<u32>>) -> Vec<u8> {
+    let mut bytes = Vec::<u8>::new();
+    bytes.append(&mut u32_bytes(x.len()));
+    for i in 0..x.len() {
+        bytes.append(&mut u32_bytes(x[i].len()));
+        for j in 0..x[i].len() {
+            bytes.append(&mut x[i][j].to_le_bytes().to_vec());
+        }
+    }
+    bytes
 }
 
 pub fn restore_vec_vec_u32(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<Vec<u32>>, ()> {
@@ -215,6 +157,81 @@ pub fn restore_vec_vec_u32(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<Vec<u32>>
     Ok(y)
 }
 
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+pub fn save_vec_bool(x: &Vec<bool>) -> Vec<u8> {
+    let mut bytes = Vec::<u8>::new();
+    bytes.append(&mut u32_bytes(x.len()));
+    for i in 0..x.len() {
+        bytes.push(if x[i] { 1 } else { 0 });
+    }
+    bytes
+}
+
+pub fn restore_vec_bool(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<bool>, ()> {
+    if *pos + 4 > x.len() {
+        return Err(());
+    }
+    let n = u32_from_bytes(&x[*pos..*pos + 4]) as usize;
+    *pos += 4;
+    if *pos + n > x.len() {
+        return Err(());
+    }
+    let mut y = vec![false; n];
+    for j in 0..n {
+        y[j] = if *pos == 1 { true } else { false };
+        *pos += 1;
+    }
+    Ok(y)
+}
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+pub fn save_vec_u32(x: &Vec<u32>) -> Vec<u8> {
+    let mut bytes = Vec::<u8>::new();
+    bytes.append(&mut u32_bytes(x.len()));
+    for i in 0..x.len() {
+        bytes.append(&mut x[i].to_le_bytes().to_vec());
+    }
+    bytes
+}
+
+pub fn restore_vec_u32(x: &Vec<u8>, pos: &mut usize) -> Result<Vec<u32>, ()> {
+    if *pos + 4 > x.len() {
+        return Err(());
+    }
+    let n = u32_from_bytes(&x[*pos..*pos + 4]) as usize;
+    *pos += 4;
+    if *pos + 4 * n > x.len() {
+        return Err(());
+    }
+    let mut y = vec![0; n];
+    for j in 0..n {
+        y[j] = u32_from_bytes(&x[*pos..*pos + 4]);
+        *pos += 4;
+    }
+    Ok(y)
+}
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+pub fn save_u32(x: u32) -> Vec<u8> {
+    x.to_le_bytes().to_vec()
+}
+
+pub fn restore_u32(x: &Vec<u8>, pos: &mut usize) -> Result<u32, ()> {
+    if *pos + 4 > x.len() {
+        return Err(());
+    }
+    let y = u32_from_bytes(&x[*pos..*pos + 4]);
+    *pos += 4;
+    Ok(y)
+}
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+const ENCLONE_VISUAL_HISTORY_VERSION: usize = 1;
+
 impl EncloneVisualHistory {
     //
     // requirements for saving and restoring history
@@ -233,14 +250,6 @@ impl EncloneVisualHistory {
     //    where *** is a positive integer, padded on the right with blanks
     // 2. data for each of the fields in EncloneVisualHistory,
     //    with translated_input_history and translated_input_hist_uniq stored first.
-    //
-    // supported subtypes
-    // - u32
-    // - Vec<u32>
-    // - Vec<bool>
-    // - Vec<String>
-    // - Vec<Vec<u8>>
-    // - Vec<Vec<u32>>
 
     pub fn save_as_bytes(&self) -> Vec<u8> {
         let mut bytes = format!(
