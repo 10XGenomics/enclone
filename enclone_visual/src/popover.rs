4a9ca330209065cc2e7fe7b0d00c3b716b3131c2
David Jaffe <david.jaffe@10xgenomics.com>
2021-08-08 10:12:12 -0700

    rustfmt

--- enclone_visual/src/popover.rs
+++ enclone_visual/src/popover.rs
@@ -118,10 +118,12 @@ pub fn archive(slf: &mut gui_structures::EncloneVisual) -> Element<Message> {
                 archive_scrollable = archive_scrollable.push(Space::with_height(Units(8)));
                 let mut row = Row::new();
                 row = row.push(Space::with_width(Units(43)));
-                row = row.push(Text::new(&format!("{}", slf.restore_msg[i]))
-                    .font(DEJAVU)
-                    .color(Color::from_rgb(1.0, 0.0, 0.0))
-                    .size(16));
+                row = row.push(
+                    Text::new(&format!("{}", slf.restore_msg[i]))
+                        .font(DEJAVU)
+                        .color(Color::from_rgb(1.0, 0.0, 0.0))
+                        .size(16),
+                );
                 archive_scrollable = archive_scrollable.push(row);
             }
             if slf.expand_archive_entry[i] {
