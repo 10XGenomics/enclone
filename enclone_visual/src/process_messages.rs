781a1751e0d0fcb8f612e3ef14600f2fbc5f7a89
David Jaffe <david.jaffe@10xgenomics.com>
2021-08-13 14:06:08 -0700

    convert error to panic

--- enclone_visual/src/process_messages.rs
+++ enclone_visual/src/process_messages.rs
@@ -504,13 +504,11 @@ impl EncloneVisual {
                         let path = format!("{}/{}", self.archive_dir.as_ref().unwrap(), x);
                         let res = read_command_list_and_name_and_origin_and_narrative(&path);
                         if res.is_err() {
-                            xprintln!(
-                                "Unable to read the history file at\n{}\n\
+                            panic!("Unable to read the history file at\n{}\n\
                                 This could either be a bug in enclone or it could be that \
                                 the file is corrupted.\n",
                                 path,
                             );
-                            std::process::exit(1);
                         }
                         let (command_list, name, origin, narrative) = res.unwrap();
                         self.archived_command_list[i] = Some(command_list);
