77fa9392bf11768dcfe365306059bf7885dd36d4
David Jaffe <david.jaffe@10xgenomics.com>
2021-08-20 11:06:11 -0700

    fix bug in test

--- enclone_visual/src/testsuite.rs
+++ enclone_visual/src/testsuite.rs
@@ -54,7 +54,6 @@ pub fn metatests() -> Vec<Vec<Message>> {
             Message::ArchiveRefresh,
             Message::ArchiveRefreshComplete(Ok(())),
             Message::ExpandArchiveEntry(true, 0),
-            Message::UserName("*****".to_string(), 0),
             Message::SetName("share"),    // test share
         ],
     ]
