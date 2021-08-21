8085d0b0f402510350f6dd580f39c9d7a8dda097
David Jaffe <david.jaffe@10xgenomics.com>
2021-08-20 17:25:19 -0700

    partially implement SPLIT_PLOT_BY_ORIGIN

--- enclone_tail/src/plot_utils.rs
+++ enclone_tail/src/plot_utils.rs
@@ -28,85 +28,102 @@ pub fn build_clusters(
 ) -> Vec<PlotCluster> {
     let mut clusters = Vec::<PlotCluster>::new();
     const SEP: f64 = 1.0; // separation between clusters
+    let mut passes = 1;
+    if plot_opt.split_plot_by_origin {
+        passes = ctl.origin_info.origin_list.len();
+    }
     for i in 0..exacts.len() {
-        let mut colors = Vec::<String>::new();
-        let mut coords = Vec::<(f64, f64)>::new();
-        let mut barcodes = Vec::<(usize, String)>::new();
-        let mut n = 0;
-
-        // For PLOT_BY_MARK, find the dataset having the largest number of cells.
-
-        let mut dsx = 0;
-        if plot_opt.plot_by_mark {
-            let mut ds = Vec::<usize>::new();
+        for pass in 0..passes {
+            let mut colors = Vec::<String>::new();
+            let mut coords = Vec::<(f64, f64)>::new();
+            let mut barcodes = Vec::<(usize, String)>::new();
+            let mut n = 0;
+    
+            // For PLOT_BY_MARK, find the dataset having the largest number of cells.
+            // Ignoring SPLIT_PLOT_BY_ORIGIN.
+    
+            let mut dsx = 0;
+            if plot_opt.plot_by_mark {
+                let mut ds = Vec::<usize>::new();
+                for j in 0..exacts[i].len() {
+                    let ex = &exact_clonotypes[exacts[i][j]];
+                    for j in 0..ex.clones.len() {
+                        ds.push(ex.clones[j][0].dataset_index);
+                    }
+                }
+                ds.sort();
+                let mut freq = Vec::<(u32, usize)>::new();
+                make_freq(&ds, &mut freq);
+                dsx = freq[0].1;
+            }
+    
+            // Go through the exact subclonotypes in a clonotype.
+    
             for j in 0..exacts[i].len() {
                 let ex = &exact_clonotypes[exacts[i][j]];
-                for j in 0..ex.clones.len() {
-                    ds.push(ex.clones[j][0].dataset_index);
+    
+                // Traverse the cells in the exact subclonotype.
+    
+                for k in 0..ex.clones.len() {
+                    if passes > 1 {
+                        let li = ex.clones[k][0].dataset_index;
+                        let p = bin_position(
+                            &ctl.origin_info.origin_list,
+                            &ctl.origin_info.origin_id[li]
+                        );
+                        if pass != p as usize {
+                            continue;
+                        }
+                    }
+                    barcodes.push((
+                        ex.clones[k][0].dataset_index,
+                        ex.clones[k][0].barcode.clone(),
+                    ));
+                    let color = assign_cell_color(
+                        &ctl,
+                        &plot_opt,
+                        &refdata,
+                        &const_names,
+                        dsx,
+                        &exacts,
+                        &exact_clonotypes,
+                        i,
+                        j,
+                        k,
+                    );
+                    colors.push(color);
+                    coords.push(hex_coord(n, 1.0));
+                    n += 1;
                 }
             }
-            ds.sort();
-            let mut freq = Vec::<(u32, usize)>::new();
-            make_freq(&ds, &mut freq);
-            dsx = freq[0].1;
-        }
-
-        // Go through the exact subclonotypes in a clonotype.
-
-        for j in 0..exacts[i].len() {
-            let ex = &exact_clonotypes[exacts[i][j]];
-
-            // Traverse the cells in the exact subclonotype.
-
-            for k in 0..ex.clones.len() {
-                barcodes.push((
-                    ex.clones[k][0].dataset_index,
-                    ex.clones[k][0].barcode.clone(),
-                ));
-                let color = assign_cell_color(
-                    &ctl,
-                    &plot_opt,
-                    &refdata,
-                    &const_names,
-                    dsx,
-                    &exacts,
-                    &exact_clonotypes,
-                    i,
-                    j,
-                    k,
-                );
-                colors.push(color);
-                coords.push(hex_coord(n, 1.0));
-                n += 1;
+    
+            // Move colors around to get vertical separation, e.g. blues on left, reds on right.
+    
+            coords.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            sort_sync2(&mut colors, &mut barcodes);
+    
+            // Substitute enclone colors.
+    
+            for j in 0..colors.len() {
+                substitute_enclone_color(&mut colors[j]);
             }
+    
+            // Save.
+    
+            let mut radius = 0.0f64;
+            for j in 0..coords.len() {
+                radius =
+                    radius.max(1.0 + (coords[j].0 * coords[j].0 + coords[j].1 * coords[j].1).sqrt());
+            }
+            radius += SEP;
+            clusters.push(PlotCluster {
+                colors: colors,
+                coords: coords,
+                clonotype_index: i,
+                barcodes: barcodes,
+                radius: radius,
+            });
         }
-
-        // Move colors around to get vertical separation, e.g. blues on left, reds on right.
-
-        coords.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        sort_sync2(&mut colors, &mut barcodes);
-
-        // Substitute enclone colors.
-
-        for j in 0..colors.len() {
-            substitute_enclone_color(&mut colors[j]);
-        }
-
-        // Save.
-
-        let mut radius = 0.0f64;
-        for j in 0..coords.len() {
-            radius =
-                radius.max(1.0 + (coords[j].0 * coords[j].0 + coords[j].1 * coords[j].1).sqrt());
-        }
-        radius += SEP;
-        clusters.push(PlotCluster {
-            colors: colors,
-            coords: coords,
-            clonotype_index: i,
-            barcodes: barcodes,
-            radius: radius,
-        });
     }
     clusters
 }
