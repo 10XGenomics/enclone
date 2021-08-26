acc231e4bddcdeeb052495366d90b167f1aa83d7
David Jaffe <david.jaffe@10xgenomics.com>
2021-08-26 05:24:11 -0700

    fix spelling error

--- enclone_tools/src/bin/import_data.rs
+++ enclone_tools/src/bin/import_data.rs
@@ -162,7 +162,7 @@ fn main() {
                     let pid = m.between("\"pipestance_id\":\"", "\"").to_string();
                     let meta = &config["meta"];
                     let url = format!("{}/{}", meta, pid);
-                    println!("gettin data for {} from {}", antibody_seq_id, url);
+                    println!("getting data for {} from {}", antibody_seq_id, url);
                     let o = Command::new("curl")
                         .arg(&url)
                         .output()
