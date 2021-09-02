e4931ad90a743a228446c56dd8f550d62ba42600
Shaun Jackman <sjackman@gmail.com>
2021-09-02 14:11:08 -0700

    Do not fail the build when git fails (#302)
    This change is needed to support building enclone outside of a Git repository (or if the git executable were not available). It is needed for some work for remote Bazel builds.

--- enclone_core/build.rs
+++ enclone_core/build.rs
@@ -46,8 +46,11 @@ fn get_commit_hash() -> String {
         .current_dir(env!("CARGO_MANIFEST_DIR"))
         .output()
         .unwrap();
-    assert!(output.status.success());
-    String::from_utf8_lossy(&output.stdout).to_string()
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).to_string()
+    } else {
+        "unknown".to_string()
+    }
 }
 
 fn is_github() -> bool {
@@ -79,10 +82,13 @@ fn get_branch_name() -> String {
         .current_dir(env!("CARGO_MANIFEST_DIR"))
         .output()
         .unwrap();
-    assert!(output.status.success());
-    String::from_utf8_lossy(&output.stdout)
-        .trim_end()
-        .to_string()
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout)
+            .trim_end()
+            .to_string()
+    } else {
+        "unknown".to_string()
+    }
 }
 
 fn is_working_tree_clean() -> bool {
