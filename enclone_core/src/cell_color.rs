fb743463a8dd2726186ff999ac83d32d8ffee493
David Jaffe <david.jaffe@10xgenomics.com>
2021-09-04 09:49:50 -0700

    add comments

--- enclone_core/src/cell_color.rs
+++ enclone_core/src/cell_color.rs
@@ -8,6 +8,8 @@
 // =sample_from_meta                            by sample, using the color field in META
 // =sample,sample1->color1,...,samplen->colorn  by sample, using the given assignment
 // =bc                                          by color to barcode assignment, via BC or META/bc
+//
+// For the default (Unspecified) schema, all cells are black.
 
 use std::collections::HashMap;
 
@@ -16,6 +18,11 @@ pub struct ColorByIsotype {
     pub color: Vec<String>,
     pub show_legend: bool,
 }
+
+// For coloring by variable value, the value of a variable is first truncated to the range
+// [min, max], then scaled to [0, 1], then converted to 0,...,255, and then converted to a color
+// using the turbo color scheme.  If the value of a variable is unspecified for a given cell,
+// or not convertible to a number, black is used.
 
 #[derive(Clone)]
 pub struct ColorByVariableValue {
