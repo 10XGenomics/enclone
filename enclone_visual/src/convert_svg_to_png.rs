f56571c26f40244044a0b4566520e7de0d1efc31
David Jaffe <david.jaffe@10xgenomics.com>
2021-09-08 09:06:33 -0700

    add function to set pixels per meter

--- enclone_visual/src/convert_svg_to_png.rs
+++ enclone_visual/src/convert_svg_to_png.rs
@@ -1,9 +1,70 @@
 // Copyright (c) 2021 10x Genomics, Inc. All rights reserved.
 
-// This code is pretty much copied from the resvg crate,
-// rev = 6b29007311edc5022635362fe56f6e5c0318fdeb, done June 14, 2021.
-
+use crc::*;
 use string_utils::*;
+
+// Modify a given PNG file by changing the pixels per meter to the given value.  This adds or
+// replaces a preexisting pHYs chunk in the file.  Note that presence of an iDOT chunk might
+// prevent the pHYs change from having an effect.  The iDOT chunk is an undocumented apple-ism.
+// Googling reveals partial descriptions of it obtained by reverse engineering.
+//
+// The only tested value for pixels per meter is 5669, which corresponds to 144 DPI.  Using this
+// value on a Mac causes the image to appear at half the size and hence at higher resolution,
+// closer to the true Mac resolution.  This was tested on one Mac and might not be fully
+// generalizable.
+//
+// References:
+// 1. https://en.wikipedia.org/wiki/Portable_Network_Graphics
+// 2. https://www.w3.org/TR/2003/REC-PNG-20031110/#11pHYs
+// 3. https://stackoverflow.com/questions/33894790/what-is-the-idot-chunk
+// 4. https://www.hackerfactor.com/blog/index.php?/archives/895-Connecting-the-iDOTs.html
+
+pub fn set_pixels_per_meter(png: &mut Vec<u8>, pixels_per_meter: u32) {
+
+    // Form the pHYs chunk.
+
+    let mut bytes = Vec::<u8>::new();
+    {
+        let mut data = Vec::<u8>::new();
+        data.append(&mut pixels_per_meter.to_be_bytes().to_vec());
+        data.append(&mut pixels_per_meter.to_be_bytes().to_vec());
+        data.append(&mut vec![1 as u8]);
+        let len = 9 as u32;
+        bytes.append(&mut len.to_be_bytes().to_vec());
+        bytes.append(&mut b"pHYs".to_vec());
+        bytes.append(&mut data);
+        let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
+        let cs = crc.checksum(&bytes[4..bytes.len()]);
+        bytes.append(&mut cs.to_be_bytes().to_vec());
+    }
+
+    // Copy the png, inserting a new pHYs chunk, and deleting the existing one.  We put the
+    // new one right after the IHDR chunk.  According to the spec, it needs to go after IHDR and
+    // before the first IDAT.
+
+    let mut png2 = png[0..8].to_vec();
+    let mut first = true;
+    let mut pos = 8;
+    while pos < png.len() {
+        let mut x = png[pos..pos + 4].to_vec();
+        let len = u32::from_be_bytes([x[0], x[1], x[2], x[3]]);
+        let total = len as usize + 12;
+        x = png[pos + 4..pos + 8].to_vec();
+        if x != b"pHYs" {
+            x = png[pos..pos + total].to_vec();
+            png2.append(&mut x);
+        }
+        if first {
+            png2.append(&mut bytes);
+            first = false;
+        }
+        pos += total;
+    }
+    *png = png2;
+}
+
+// The following code is pretty much copied from the resvg crate,
+// rev = 6b29007311edc5022635362fe56f6e5c0318fdeb, done June 14, 2021.
 
 pub fn convert_svg_to_png(svg: &[u8]) -> Vec<u8> {
     let fontdb = load_fonts();
