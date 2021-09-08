85dacb575d885691b7bdfc5e4018d73ee56f3206
David Jaffe <david.jaffe@10xgenomics.com>
2021-09-08 16:44:56 -0700

    update comments

--- enclone_core/src/cell_color.rs
+++ enclone_core/src/cell_color.rs
@@ -2,14 +2,14 @@
 //
 // Define the coloring scheme for cells.
 //
-// CELL_COLOR
-// =isotype,color1,...,colorn                   by isotype; uses default colors if none provided
-// =var,name,min,max                            by values of given variable
-// =sample_from_meta                            by sample, using the color field in META
-// =sample,sample1->color1,...,samplen->colorn  by sample, using the given assignment
-// =bc                                          by color to barcode assignment, via BC or META/bc
-//
-// For the default (Unspecified) schema, all cells are black.
+// color_spec:
+// - const,color                              all cells are assigned the given color (default/black)
+// - iso,color1,...,colorn                    by isotype; uses default colors if none provided
+// - var,name,minmax,min,max                  by values of given variable
+// - dataset                                  by dataset, using the color field in META
+// - origin,origin1,color1,...,originn,colorn by origin, using the given assignment
+// - bc                                       by color to barcode assignment, via BC or META/bc
+// - mark                                     (internal)
 
 use std::collections::HashMap;
 
