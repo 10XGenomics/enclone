4a3176cb1fedffb7835748ab8203a203ee72edb4
David Jaffe <david.jaffe@10xgenomics.com>
2021-09-11 05:59:06 -0700

    add test

--- enclone_visual/src/testsuite.rs
+++ enclone_visual/src/testsuite.rs
@@ -99,6 +99,14 @@ pub fn metatests() -> Vec<Vec<Message>> {
             ),
             Message::SubmitButtonPressed(Ok(())),
             Message::SetName("trunc"),
+            Message::InputChanged1(
+                "enclone BCR=85333 HONEY=out=gui,color=var,u_cell1,legend=none"
+                .to_string()
+            ),
+            Message::SubmitButtonPressed(Ok(())),
+            Message::SetName("no_legend"),
+
+
         ],
     ]
 }
