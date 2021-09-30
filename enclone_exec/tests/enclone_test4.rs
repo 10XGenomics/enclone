2c2b516623dc1dab1db3d7bffa7e837f389cb7c6
Adam Azarchs <adam.azarchs@10xgenomics.com>
2021-09-30 13:27:24 -0700

    Remove unused dependencies. (#319)
    * Remove unused dependencies.
    
    * Update crates for security advisories.
    
    * Enable sse4 optimizations, as in cellranger.
    
    * Fix test for number of duplicated crates.

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -186,7 +186,7 @@ fn test_dupped_crates() {
     unique_sort(&mut crates);
     let n2 = crates.len();
     let d = n1 - n2;
-    const DUPPED_CRATES: usize = 38;
+    const DUPPED_CRATES: usize = 33;
     if d != DUPPED_CRATES {
         eprintln!(
             "\nThe number of duplicated crates is {}, but the required number is {}.\n",
