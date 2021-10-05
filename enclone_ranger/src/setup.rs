efc3c4b78fd052048a2dd26b93de4c9cc14bda56
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-05 12:01:26 -0700

    delete unneeded stuff

--- enclone_ranger/src/setup.rs
+++ enclone_ranger/src/setup.rs
@@ -2,7 +2,6 @@
 
 use crate::USING_PAGER;
 use enclone_args::proc_args::proc_args;
-use enclone_args::proc_args2::is_simple_arg;
 use enclone_core::defs::EncloneControl;
 use std::sync::atomic::Ordering::SeqCst;
 use vector_utils::erase_if;
@@ -12,13 +11,12 @@ pub fn setup(
     args: &Vec<String>,
     argsx: &mut Vec<String>,
 ) -> Result<(), String> {
-    let using_pager = false;
+    ctl.gen_opt.nopager = true;
     {
         let mut args = args.clone();
         let mut to_delete = vec![false; args.len()];
         for i in 1..args.len() {
             if args[i] == "NOPAGER" {
-                ctl.gen_opt.nopager = true;
                 to_delete[i] = true;
             } else if args[i] == "FORCE_EXTERNAL" {
                 to_delete[i] = true;
@@ -31,17 +29,9 @@ pub fn setup(
         erase_if(&mut args, &to_delete);
         *argsx = args.clone();
     }
-
-    // Pretest for some options.
-
     ctl.pretty = true;
     ctl.gen_opt.h5 = true;
-    for i in 1..args.len() {
-        if is_simple_arg(&args[i], "PLAIN")? {
-            ctl.pretty = false;
-        }
-    }
-    USING_PAGER.store(using_pager, SeqCst);
+    USING_PAGER.store(false, SeqCst);
 
     // Process args (and set defaults for them).
 
