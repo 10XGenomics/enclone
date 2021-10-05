0e593263d3e6646fb11efc05f9b04c63b494699a
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-05 12:07:16 -0700

    delete unneeded stuff

--- enclone_ranger/src/lib.rs
+++ enclone_ranger/src/lib.rs
@@ -1,7 +1,6 @@
 // Copyright (c) 2021 10x Genomics, Inc. All rights reserved.
 
 pub mod main_enclone;
-pub mod setup;
 pub mod stop;
 
 use std::sync::atomic::AtomicBool;
--- enclone_ranger/src/main_enclone.rs
+++ enclone_ranger/src/main_enclone.rs
@@ -3,10 +3,11 @@
 // See README for documentation.
 
 use self::refx::{make_vdj_ref_data_core, RefData};
-use crate::setup::setup;
 use crate::stop::main_enclone_stop;
+use crate::USING_PAGER;
 use enclone::innate::species;
 use enclone_args::load_gex::get_gex_info;
+use enclone_args::proc_args::proc_args;
 use enclone_core::defs::EncloneControl;
 use enclone_core::enclone_structs::*;
 use enclone_stuff::start::*;
@@ -16,6 +17,7 @@ use std::{
     io::{BufRead, BufReader},
     time::Instant,
 };
+use std::sync::atomic::Ordering::SeqCst;
 use string_utils::TextUtils;
 use vdj_ann::refx;
 
@@ -46,12 +48,14 @@ pub fn main_enclone_setup(args: &Vec<String>) -> Result<EncloneSetup, String> {
     ctl.start_time = Some(tall);
     ctl.gen_opt.cpu_all_start = 0;
     ctl.gen_opt.cpu_this_start = 0;
-    let mut argsx = Vec::<String>::new();
-    setup(&mut ctl, &args, &mut argsx)?;
+    ctl.gen_opt.nopager = true;
+    ctl.pretty = true;
+    ctl.gen_opt.h5 = true;
+    USING_PAGER.store(false, SeqCst);
+    proc_args(&mut ctl, args)?;
     let mut argsy = Vec::<String>::new();
     for i in 0..args_orig.len() {
-        if args_orig[i] != "HTML"
-            && args_orig[i] != "NOPAGER"
+        if args_orig[i] != "NOPAGER"
             && args_orig[i] != "FORCE_EXTERNAL"
             && !args_orig[i].starts_with("PRE=")
             && !args_orig[i].starts_with("MAX_CORES=")
