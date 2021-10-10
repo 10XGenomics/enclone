311d3d14c968e08657d7d475da2cd76864d17cdc
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-10 06:26:24 -0700

    tweak

--- enclone_tools/src/feature_barcode_matrix.rs
+++ enclone_tools/src/feature_barcode_matrix.rs
@@ -311,7 +311,7 @@ pub fn feature_barcode_matrix(
                 } else {
                     fb = s[10..25].to_vec();
                     if verbosity == 2 {
-                        println!("{} {} {} {} {} {}",
+                        println!("r: {} {} {} {} {} {}",
                             strme(&barcode),
                             strme(&umi),
                             strme(&s[0..10]),
