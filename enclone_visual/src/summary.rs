2620c91555c10e166de864ef33dd9d6556ce5e72
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-11 09:27:53 -0700

    add to doc

--- enclone_visual/src/summary.rs
+++ enclone_visual/src/summary.rs
@@ -434,6 +434,25 @@ pub fn summary(slf: &mut gui_structures::EncloneVisual) -> Element<Message> {
             )
             .push(Space::with_height(Units(8)))
             .push(Text::new(
+                "For each dataset, we show a table that classifies its feature barcode reads.\n\n\
+                 \
+                 These reads are first classified as cellular, if their cell barcode was \
+                 identified as a cell by the Cell Ranger VDJ pipeline, or else noncellular.\n\n\
+                 \
+                 Each of these categories is further divided into three subcategories:\n\
+                 • degenerate: these are the case where R2 starts with at least ten Gs\n\
+                 • reference: nondegenerate, and the feature barcode is in the feature barcode \
+                 reference\n\
+                 • nonreference: nondegenerate, and whose feature barcode not in the reference.\n\n\
+                 \
+                 In the degenerate category, we identify canonical reads, which we define as those \
+                 whose R1 contains the sequence CACATCTCCGAGCCCACGAGAC.  This is the end of the \
+                 Illumina Nextera version of the R2 primer = CTGTCTCTTATACACATCTCCGAGCCCACGAGAC.  \
+                 If R1 contains the first ten bases = CACATCTCCG, and the read is not canonical, \
+                 we call it semicanonical.",
+            ))
+            .push(Space::with_height(Units(8)))
+            .push(Text::new(
                 "All the tables can be copied at once, in a form suitable for inclusion in \
                  a spreadsheet, by pushing the button below.  This copies the numbers in the \
                  last column, but not the numbers in the earlier columns.",
