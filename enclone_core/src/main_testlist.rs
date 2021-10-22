9d988bfe727e001793779fac031bebc0ebe72d15
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-22 13:12:12 -0700

    replace test; ./test passes

--- enclone_core/src/main_testlist.rs
+++ enclone_core/src/main_testlist.rs
@@ -1,6 +1,6 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
-pub const TESTS: [&str; 256] = [
+pub const TESTS: [&str; 257] = [
     // 1. tests variant base after CDR3, parseable output
     r###"BCR=123089 CDR3=CVRDRQYYFDYW POUT=stdout
      PCOLS=exact_subclonotype_id,n,v_name1,v_name2,nchains,var_indices_aa1,barcodes"###,
@@ -643,4 +643,6 @@ pub const TESTS: [&str; 256] = [
     // This is from BCR="131036;140707".
     r###"PRE=testx/inputs BCR="flaky8a;flaky8b" ANN SHOW_BC FAIL_ONLY=true
          PRINT_FAILED_JOINS MIX_DONORS BUILT_IN NO_PRE"###,
+    // 257. clonotype that was two clonotypes before raising MAX_DIFFS to 60, from 1084461-1084462
+    r###"BCR=testx/inputs/flaky CDR3=CAKEFGNGGFDTFDIW BUILT_IN AMINO=cdr3"###,
 ];
--- enclone_core/src/testlist.rs
+++ enclone_core/src/testlist.rs
@@ -137,7 +137,7 @@ pub const CRASH_SETS: [&str; 6] = [
 // Test using datasets that are either in the extended public dataset collection, or which are
 // not publicly avaiable, or which require samtools.
 
-pub const EXTENDED_TESTS: [&str; 23] = [
+pub const EXTENDED_TESTS: [&str; 22] = [
     // 1. test DVARS
     r###"TCR_GEX=1175300-1175301 DVARS=Ag_PE-C0951_ab_cellular_u,Ag_PE-C0951_ab_cellular_r
          NOPRINT SUMMARY SUMMARY_CLEAN NFORCE"###,
@@ -200,9 +200,7 @@ pub const EXTENDED_TESTS: [&str; 23] = [
     // 21. the result of this changed when sub_alts was changed
     r###"BCR="40086;132888" SEG=IGHV3-43 MIX_DONORS MAX_DIFFS=80 CDR3=CVKGDWGSAFDIW
          NO_PRE NFORCE"###,
-    // 22. clonotype that was two clonotypes before raising MAX_DIFFS to 60
-    r###"BCR=1084461-1084462 CDR3=CAKEFGNGGFDTFDIW NO_PRE NFORCE"###,
-    // 23. test BCR_GEX and GD_BC
+    // 22. test BCR_GEX and GD_BC
     r###"BCR_GEX=1089851 GD_BC=1089848 NOPRINT NO_PRE NFORCE EXPECT_OK"###,
 ];
 
