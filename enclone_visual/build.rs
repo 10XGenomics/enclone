ac37754708dbf1bd788aecf9b650697ac009d7b0
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-28 07:15:07 -0700

    Revert "Revert "Revert "Add rerun-if directives to build.rs scripts."""
    This reverts commit 7f4aa34bd7ae51cc155df8f2cd480bfd8a47ac51.

--- enclone_visual/build.rs
+++ enclone_visual/build.rs
@@ -1,7 +1,6 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=server.proto");
     // compiling protos using path on build time
 
     tonic_build::compile_protos("./server.proto")?;
