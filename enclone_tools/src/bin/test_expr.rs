950718667f0fc12878ebfa1900f9320968084049
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-28 11:12:11 -0700

    add some comments

--- enclone_tools/src/bin/test_expr.rs
+++ enclone_tools/src/bin/test_expr.rs
@@ -135,7 +135,12 @@ pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalex
 // ================================================================================================
 
 fn main() {
+    // Define an expression.
+
     let expr = "prod(x, y)";
+
+    // Compile the expression.
+
     let compiled = build_operator_tree(&expr); // creates a Node
     if compiled.is_err() {
         println!("failed");
@@ -143,9 +148,18 @@ fn main() {
         println!("ok");
     }
     let compiled = compiled.unwrap();
+
+    // Create variables and values.
+
     let vars = vec!["x".to_string(), "y".to_string()];
     let vals = vec!["1.5".to_string(), "100".to_string()];
+
+    // Set up compute context.
+
     let c = define_evalexpr_context(&vars, &vals);
+
+    // Determine the value of the expression in the compute context.
+
     let res = compiled.eval_with_context(&c);
     if res.is_err() {
         println!("evaluation failed");
