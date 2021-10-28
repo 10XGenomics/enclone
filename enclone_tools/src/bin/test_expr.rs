bab0cae9c65d2aeb9b9467b4a176c58b5abc2341
David Jaffe <david.jaffe@10xgenomics.com>
2021-10-28 08:08:59 -0700

    work in progress

--- enclone_tools/src/bin/test_expr.rs
+++ enclone_tools/src/bin/test_expr.rs
@@ -1,16 +1,22 @@
 // Copyright (c) 2021 10X Genomics, Inc. All rights reserved.
 
+// Experimental code for working with evalexpr.
 
 use evalexpr::build_operator_tree;
 use evalexpr::{ContextWithMutableFunctions, ContextWithMutableVariables, HashMapContext};
 use evalexpr::Function;
 use evalexpr::Value;
+use string_utils::*;
 
 // ================================================================================================
 
 // Given a a list of variables and values for them, define an evalexpr::HashMapContext that
 // includes these variables, as well as some convenient (but arbitrarily chosen) functions.
 // This can then be used to evaluate an expression.
+//
+// Functions take as input zero or more f64 arguments, and return an f64.  The machine allows them
+// to be called on arbitrary strings, but if the strings are not all f64, then the return value is 
+// null.
 
 pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalexpr::HashMapContext {
     assert_eq!(vars.len(), vals.len());
@@ -19,7 +25,11 @@ pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalex
     // Define the variable values.
 
     for i in 0..vars.len() {
-        c.set_value(vars[i].clone(), evalexpr::Value::String(vals[i].clone())).unwrap();
+        if vals[i].parse::<f64>().is_ok() {
+            c.set_value(vars[i].clone(), evalexpr::Value::Float(vals[i].force_f64())).unwrap();
+        } else {
+            c.set_value(vars[i].clone(), evalexpr::Value::String(vals[i].clone())).unwrap();
+        }
     }
 
     // Define a function.
@@ -42,7 +52,7 @@ pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalex
                     }
                 }
             }
-            Ok(Value::from("undefined"))
+            Ok(Value::from(""))
         })
     );
 
@@ -58,7 +68,7 @@ pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalex
                 let x = x.as_number().unwrap(); 
                 Ok(Value::from(square(x)))
             } else {
-                Ok(Value::from("undefined"))
+                Ok(Value::from(""))
             }
         })
     );
@@ -71,19 +81,6 @@ pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalex
 // ================================================================================================
 
 fn main() {
-
-    // let expr = "3 + 5";
-    // let expr = "3+5";
-    // let expr = "f(arglin-9,7,pot(moo)) + 5";
-    // let expr = "3 > 5";
-    // let expr = "3>5";
-    // let expr = "f(10) - g(2)";
-    // let expr = "(lambdax + 5) / 0";
-    // let expr = "square(5)";
-    // let expr = "square(5.0)";
-    // let expr = "prod(2, 3)";
-
-
 
     let expr = "prod(x, y)";
     let compiled = build_operator_tree(&expr); // creates a Node
@@ -102,76 +99,4 @@ fn main() {
     }
     let res = res.unwrap();
     println!("value is {}", res);
-    if true { std::process::exit(0); }
-
-
-
-    let mut c = HashMapContext::new();
-    let _ = c.set_value("lambdax".to_string(), evalexpr::Value::Float(1.234));
-
-
-
-
-    fn square(x: f64) -> f64 {
-        x * x
-    }
-
-    let _ = c.set_function(
-        "square".to_string(), 
-        Function::new(|x| { 
-            if x.is_number() {
-                let x = x.as_number().unwrap(); 
-                Ok(Value::from(square(x)))
-            } else {
-                Ok(Value::from("undefined"))
-            }
-        })
-    );
-
-
-
-
-    fn prod(x: f64, y: f64) -> f64 {
-        x * y
-    }
-
-    let _ = c.set_function(
-        "prod".to_string(), 
-        Function::new(|t| { 
-            if t.is_tuple() {
-                let t = t.as_tuple().unwrap();
-                if t.len() == 2 {
-                    let x = &t[0];
-                    let y = &t[1];
-                    if x.is_number() && y.is_number() {
-                        let x = x.as_number().unwrap(); 
-                        let y = y.as_number().unwrap(); 
-                        return Ok(Value::from(prod(x, y)));
-                    }
-                }
-            }
-            Ok(Value::from("undefined"))
-        })
-    );
-
-
-
-
-    let res = compiled.eval_with_context(&c);
-    if res.is_err() {
-        println!("evaluation failed");
-    }
-    let res = res.unwrap();
-    println!("value is {}", res);
 }
-
-/*
-
-• use == for equality, and not =
-• put string values in single quotes 
-• put the entire expression in double quotes
-
-enclone BCR=123085 BC=f KEEP_CELL_IF="nice == 'true'" 
-enclone BCR=123085 BC=f KEEP_CELL_IF="nice == 'true' && rank <= 5"
-
-*/
