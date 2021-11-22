02fae000f510bdd0d42a1209e0e1a5247b5c80c1
David Jaffe <david.jaffe@10xgenomics.com>
2021-11-22 15:52:47 -0800

    require compatable versions in test_vis

--- enclone_visual/src/enclone_client.rs
+++ enclone_visual/src/enclone_client.rs
@@ -75,6 +75,7 @@ pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error
     let mut monitor_threads = false;
     let mut config_name = String::new();
     let mut fixed_port = None;
+    let mut require_compatible = false;
     for i in 1..args.len() {
         let arg = &args[i];
 
@@ -110,6 +111,8 @@ pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error
             META.store(arg.after("META=").force_usize() - 1, SeqCst);
         } else if arg.starts_with("EXEC=") {
             EXEC.lock().unwrap().push(arg.after("EXEC=").to_string());
+        } else if arg == "REQUIRE_COMPATIBLE" {
+            require_compatible = true;
         } else {
             xprintln!(
                 "\nCurrently the only allowed arguments are VIS, VIS=x where x is a\n\
@@ -645,6 +648,9 @@ pub async fn enclone_client(t: &Instant) -> Result<(), Box<dyn std::error::Error
                 } else if verbose {
                     xprintln!("local version = {}", local);
                     xprintln!("remote version = {}", remote);
+                    if require_compatible {
+                        std::process::exit(1);
+                    }
                 }
             }
         }
