1c5c794d6fe2964d515c950685b77942b93820a6
David Jaffe <david.jaffe@10xgenomics.com>
2021-11-23 12:10:23 -0800

    accept speedup

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -91,7 +91,7 @@ fn test_cpu_usage() {
             gi = line.force_f64() / 1_000_000_000.0;
         }
     }
-    const REQUIRED_GI: f64 = 18.7095;
+    const REQUIRED_GI: f64 = 18.5000;
     let err = ((gi - REQUIRED_GI) / REQUIRED_GI).abs();
     let report = format!(
         "Observed GI = {:.4}, versus required GI = {:.4}, err = {:.2}%, versus max \
