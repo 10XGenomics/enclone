cc984ee6275592aaad2d56ee491e09a40e89ec58
David Jaffe <david.jaffe@10xgenomics.com>
2021-12-15 05:50:36 -0800

    record speed improvement of 4.77%; also update Cargo.lock

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -94,7 +94,7 @@ fn test_cpu_usage() {
             gi = line.force_f64() / 1_000_000_000.0;
         }
     }
-    const REQUIRED_GI: f64 = 56.7080;
+    const REQUIRED_GI: f64 = 54.0043;
     let err = ((gi - REQUIRED_GI) / REQUIRED_GI).abs();
     let report = format!(
         "Observed GI = {:.4}, versus required GI = {:.4}, err = {:.2}%, versus max \
