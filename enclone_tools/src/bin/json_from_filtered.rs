2b3854bdf82b03044aa8602a5b4e681ce1ed5720
David Jaffe <david.jaffe@10xgenomics.com>
2022-02-20 03:42:16 -0800

    now complete, in principle

--- enclone_tools/src/bin/json_from_filtered.rs
+++ enclone_tools/src/bin/json_from_filtered.rs
@@ -9,12 +9,10 @@
 
 use io_utils::*;
 use pretty_trace::PrettyTrace;
-// use serde_json::Value;
 use std::collections::HashMap;
 use std::io::BufRead;
-// use std::io::Write;
+use std::io::Write;
 use string_utils::*;
-// use vector_utils::*;
 
 pub struct Contig {
     pub barcode: String,
@@ -66,4 +64,23 @@ fn main() {
             contigs[to_tig[&name]].seq = s.as_bytes().to_vec();
         }
     }
+    let mut f = open_for_write_new!["all_contig_annotations.json"];
+    fwriteln!(f, "[");
+    for i in 0..contigs.len() {
+        fwriteln!(f, "    {{");
+        fwriteln!(f, "\"barcode\": \"{}\",", contigs[i].barcode);
+        fwriteln!(f, "\"contig_id\": \"{}\",", contigs[i].contig_id);
+        fwriteln!(f, "\"is_cell\": {},", contigs[i].is_cell);
+        fwriteln!(f, "\"high_confidence\": {},", contigs[i].high_confidence);
+        fwriteln!(f, "\"productive\": {},", contigs[i].productive);
+        fwriteln!(f, "\"read_count\": {},", contigs[i].reads);
+        fwriteln!(f, "\"umi_count\": {},", contigs[i].umis);
+        fwriteln!(f, "\"seq\": \"{}\",", strme(&contigs[i].seq));
+        if i < contigs.len() - 1 {
+            fwriteln!(f, "    }},");
+        } else {
+            fwriteln!(f, "    }}");
+        }
+    }
+    fwriteln!(f, "]");
 }
