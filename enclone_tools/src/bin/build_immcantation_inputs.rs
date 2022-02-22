a95436a702534ce76a92eb60e0742e115895324e
David Jaffe <david.jaffe@10xgenomics.com>
2022-02-22 09:29:20 -0800

    delete dead stuff

--- enclone_tools/src/bin/build_immcantation_inputs.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 10X Genomics, Inc. All rights reserved.
-
-// See main_build_immcantation_inputs.
-
-use enclone_tools::main_build_immcantation_inputs::main_build_immcantation_inputs;
-
-pub fn main() {
-    main_build_immcantation_inputs()
-}
