396e2d6a0f98465844b4cb064dc00bb5f305a9ef
David Jaffe <david.jaffe@10xgenomics.com>
2022-02-25 12:48:03 -0800

    fix bug in use of JOIN_BASIC with MIN_CHAINS_EXACT

--- enclone/src/misc2.rs
+++ enclone/src/misc2.rs
@@ -410,8 +410,10 @@ pub fn find_exact_subclonotypes(
 
         // Save exact subclonotype.
 
-        if !clones.is_empty() {
-            res.1.push(ExactClonotype { share, clones });
+        if share.len() >= ctl.gen_opt.min_chains_exact || ctl.join_alg_opt.basic.is_none() {
+            if !clones.is_empty() {
+                res.1.push(ExactClonotype { share, clones });
+            }
         }
     });
     ctl.perf_stats(&t, "finding exact subclonotypes two");
