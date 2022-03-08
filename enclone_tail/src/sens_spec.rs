dcc97fe1ecc6210c36c42ad17acd0d6e14ad1d99
David Jaffe <david.jaffe@10xgenomics.com>
2022-03-08 12:43:03 -0800

    add 80% filter

--- enclone_tail/src/sens_spec.rs
+++ enclone_tail/src/sens_spec.rs
@@ -27,49 +27,48 @@ pub fn sens_spec(
     let mut cross_mergeable = 0;
     let mut intra_merged = 0;
     let mut cross_merged = 0;
-    let mut nnlld = Vec::<(String, String, usize, usize, usize)>::new();
+    // (heavy V+J name, light V+J name, CDR3H_len, CDR3L_len, donor, CDR3H_dna)
+    let mut nnlld = Vec::<(String, String, usize, usize, usize, Vec<u8>)>::new();
     for i in 0..exacts.len() {
         let mut donors = Vec::<usize>::new();
         for j in 0..exacts[i].len() {
             let ex = &exact_clonotypes[exacts[i][j]];
-            let mut heavy = Vec::<(String, usize)>::new();
-            let mut light = Vec::<(String, usize)>::new();
-            for i in 0..ex.share.len() {
-                let mut vname = refdata.name[ex.share[i].v_ref_id].clone();
-                if vname.contains("*") {
-                    vname = vname.before("*").to_string();
-                }
-                let mut jname = refdata.name[ex.share[i].j_ref_id].clone();
-                if jname.contains("*") {
-                    jname = jname.before("*").to_string();
-                }
-                let vj = format!("{}+{}", vname, jname);
-                if ex.share[i].left {
-                    heavy.push((vj, ex.share[i].cdr3_aa.len()));
-                } else {
-                    light.push((vj, ex.share[i].cdr3_aa.len()));
-                }
-            }
-            unique_sort(&mut heavy);
-            unique_sort(&mut light);
-            if heavy.len() > 0 || light.len() > 0 {
-                for j in 0..ex.clones.len() {
-                    if ex.clones[j][0].donor_index.is_some() {
-                        donors.push(ex.clones[j][0].donor_index.unwrap());
+            for i1 in 0..ex.share.len() {
+                if ex.share[i1].left {
+                    let mut vname = refdata.name[ex.share[i1].v_ref_id].clone();
+                    if vname.contains("*") {
+                        vname = vname.before("*").to_string();
                     }
-                }
-            }
-            for i1 in 0..heavy.len() {
-                for i2 in 0..light.len() {
-                    for j in 0..ex.clones.len() {
-                        if ex.clones[j][0].donor_index.is_some() {
-                            nnlld.push((
-                                heavy[i1].0.clone(),
-                                light[i2].0.clone(),
-                                heavy[i1].1,
-                                light[i2].1,
-                                ex.clones[j][0].donor_index.unwrap(),
-                            ));
+                    let mut jname = refdata.name[ex.share[i1].j_ref_id].clone();
+                    if jname.contains("*") {
+                        jname = jname.before("*").to_string();
+                    }
+                    let vjh = format!("{}+{}", vname, jname);
+                    for i2 in 0..ex.share.len() {
+                        if !ex.share[i2].left {
+                            let mut vname = refdata.name[ex.share[i2].v_ref_id].clone();
+                            if vname.contains("*") {
+                                vname = vname.before("*").to_string();
+                            }
+                            let mut jname = refdata.name[ex.share[i2].j_ref_id].clone();
+                            if jname.contains("*") {
+                                jname = jname.before("*").to_string();
+                            }
+                            let vjl = format!("{}+{}", vname, jname);
+                            for k in 0..ex.clones.len() {
+                                let donor = ex.clones[k][0].donor_index;
+                                if donor.is_some() {
+                                    nnlld.push((
+                                        vjh.clone(),
+                                        vjl.clone(),
+                                        ex.share[i1].cdr3_aa.len(),
+                                        ex.share[i2].cdr3_aa.len(),
+                                        donor.unwrap(),
+                                        ex.share[i1].cdr3_dna.clone().as_bytes().to_vec(),
+                                    ));
+                                    donors.push(donor.unwrap());
+                                }
+                            }
                         }
                     }
                 }
@@ -104,22 +103,21 @@ pub fn sens_spec(
             }
             j += 1;
         }
-        let mut donors = Vec::<usize>::new();
-        for k in i..j {
-            donors.push(nnlld[k].4);
-        }
-        donors.sort();
-        let mut freq = Vec::<(u32, usize)>::new();
-        make_freq(&donors, &mut freq);
-        for m in 0..freq.len() {
-            let n = freq[m].0 as usize;
-            intra_mergeable += (n * (n - 1)) / 2;
-        }
-        for m1 in 0..freq.len() {
-            for m2 in m1 + 1..freq.len() {
-                let n1 = freq[m1].0 as usize;
-                let n2 = freq[m2].0 as usize;
-                cross_mergeable += n1 * n2;
+        for k1 in i..j {
+            for k2 in k1 + 1..j {
+                let mut matches = 0;
+                for u in 0..nnlld[k1].5.len() {
+                    if nnlld[k1].5[u] == nnlld[k2].5[u] {
+                        matches += 1;
+                    }
+                }
+                if matches as f64 / nnlld[k1].5.len() as f64 >= 0.8 {
+                    if nnlld[k1].4 == nnlld[k2].4 {
+                        intra_mergeable += 1;
+                    } else {
+                        cross_mergeable += 1;
+                    }
+                }
             }
         }
         i = j;
@@ -151,8 +149,8 @@ pub fn sens_spec(
     let error_rate = cross_merged as f64 / cross_mergeable as f64;
     fwriteln!(
         log,
-        "   • error rate = {:.2} x 10^-6",
-        error_rate * 1_000_000.0
+        "   • error rate = {:.2} x 10^-3",
+        error_rate * 1_000.0
     );
     let inferred_errors = (error_rate * intra_mergeable as f64).round() as usize;
     fwriteln!(
@@ -160,10 +158,19 @@ pub fn sens_spec(
         "   • inferred errors = {}",
         add_commas(inferred_errors)
     );
-    fwriteln!(
-        log,
-        "   • adjusted true merges = {}",
-        add_commas(intra_merged - inferred_errors)
-    );
+    let adjusted = intra_merged as isize - inferred_errors as isize;
+    if adjusted >= 0 {
+        fwriteln!(
+            log,
+            "   • adjusted true merges = {}",
+            add_commas(adjusted as usize)
+        );
+    } else {
+        fwriteln!(
+            log,
+            "   • adjusted true merges = -{}",
+            add_commas(-adjusted as usize)
+        );
+    }
     stringme(&log)
 }
