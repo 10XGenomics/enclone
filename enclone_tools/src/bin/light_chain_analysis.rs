4ed9e68ee77336648e76cc983db40746d30262fc
David Jaffe <david.jaffe@10xgenomics.com>
2022-03-13 06:09:00 -0700

    work in progress

--- enclone_tools/src/bin/light_chain_analysis.rs
+++ enclone_tools/src/bin/light_chain_analysis.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 10X Genomics, Inc. All rights reserved.
 
 // Analyze light chains.  Supply a single file of data, with one line per cell, and fields
-// including donors,v_name1,v_name2,dref,cdr3_aa1,clonotype_ncells.
+// including donors_cell,v_name1,v_name2,dref,cdr3_aa1,clonotype_ncells.
 
 use pretty_trace::PrettyTrace;
 use io_utils::*;
@@ -28,7 +28,7 @@ fn main() {
             for i in 0..fields.len() {
                 tof.insert(fields[i].to_string(), i);
             }
-            assert!(tof.contains_key("donors"));
+            assert!(tof.contains_key("donors_cell"));
             assert!(tof.contains_key("v_name1"));
             assert!(tof.contains_key("v_name2"));
             assert!(tof.contains_key("dref"));
@@ -39,11 +39,23 @@ fn main() {
             data.push((
                 fields[tof["v_name1"]].to_string(),
                 fields[tof["cdr3_aa1"]].to_string(),
-                fields[tof["donors"]].to_string(),
+                fields[tof["donors_cell"]].to_string(),
                 fields[tof["v_name2"]].to_string(),
                 fields[tof["dref"]].force_usize(),
                 fields[tof["clonotype_ncells"]].force_usize(),
             ));
+        }
+    }
+    for i in 0..data.len() {
+        if data[i].3.contains("H") {
+            println!("{}, {}, {}, {}, {}, {}",
+                data[i].0,
+                data[i].1,
+                data[i].2,
+                data[i].3,
+                data[i].4,
+                data[i].5,
+            );
         }
     }
     data.sort();
@@ -63,11 +75,20 @@ fn main() {
             for k2 in i+1..j {
                 if data[k1].2 != data[k2].2 {
                     let lmatch = data[k1].3 == data[k2].3;
-                    // let (dref1, dref2) = (data[k1].4, data[k2].4);
+                    let (dref1, dref2) = (data[k1].4, data[k2].4);
                     let (cncells1, cncells2) = (data[k1].5, data[k2].5);
                     // if dref1 >= 30 && dref2 >= 30 {
                     // if dref1 == 0 && dref2 == 0 {
-                    if cncells1 >= 10 && cncells2 >= 10 {
+                    // if cncells1 >= 10 && cncells2 >= 10 {
+                    // if cncells1 == 1 && cncells2 == 1 && dref1 >= 20 && dref2 >= 20 { // 68.1%
+                    // if cncells1 == 1 && cncells2 == 1 && dref1 >= 10 && dref2 >= 10 { // 71.6%
+                    // if cncells1 == 1 && cncells2 == 1 && dref1 == 0 && dref2 == 0 { // 8.7%
+                    println!("L = {}/{}, csize = {}/{}, dref = {}/{}",
+                        data[k1].3, data[k2].3,
+                        data[k1].5, data[k2].5,
+                        data[k1].4, data[k2].4,
+                    );
+                    if true {
                         all += 1;
                         if lmatch {
                             same += 1;
