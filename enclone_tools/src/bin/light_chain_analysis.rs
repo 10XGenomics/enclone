399a1a346c40a81840d321536350e09aac50c885
David Jaffe <david.jaffe@10xgenomics.com>
2022-03-15 06:27:50 -0700

    rustfmt

--- enclone_tools/src/bin/light_chain_analysis.rs
+++ enclone_tools/src/bin/light_chain_analysis.rs
@@ -32,8 +32,17 @@ fn main() {
     let mut first = true;
     let mut tof = HashMap::<String, usize>::new();
     // data = {(v_name1, cdr3_len, cdr3_aa1, donor, v_name2, dref, clonotype_ncells, const1, hcomp)}
-    let mut data 
-        = Vec::<(String, usize, Vec<u8>, String, String, usize, usize, String, usize)>::new();
+    let mut data = Vec::<(
+        String,
+        usize,
+        Vec<u8>,
+        String,
+        String,
+        usize,
+        usize,
+        String,
+        usize,
+    )>::new();
     for line in f.lines() {
         let s = line.unwrap();
         if s.starts_with("#") {
@@ -104,7 +113,7 @@ fn main() {
                 // require different donors
                 if data[k1].3 != data[k2].3 {
                     let mut same = 0;
-                    for m in 0.. data[k1].2.len() {
+                    for m in 0..data[k1].2.len() {
                         if data[k1].2[m] == data[k2].2[m] {
                             same += 1;
                         }
@@ -142,7 +151,8 @@ fn main() {
 
     // Print results.
 
-    println!("\nConsider two cells from different donors that have the same heavy chain gene name\n\
+    println!(
+        "\nConsider two cells from different donors that have the same heavy chain gene name\n\
         and CDR3H length."
     );
     println!("\npercent identity on CDR3H-AA ==> probability that light chain genes are the same");
@@ -163,70 +173,66 @@ fn main() {
     }
     println!("");
 }
-                    
 
-
-
-    /*
-    let mut all1 = 0;
-    let mut same1 = 0;
-    let mut all2 = 0;
-    let mut same2 = 0;
-    let mut mall1 = vec![vec![0; 4]; 4];
-    let mut mall2 = vec![vec![0; 4]; 4];
-    let mut msame1 = vec![vec![0; 4]; 4];
-    let mut msame2 = vec![vec![0; 4]; 4];
-    let mut total_hcomp1 = 0;
-    let mut total_hcomp2 = 0;
-    let mut i = 0;
-    while i < data.len() {
-        // let j = next_diff12_8(&data, i as i32) as usize;
-        let mut j = i + 1;
-        while j < data.len() {
-            if data[j].0 != data[i].0 || data[j].1 != data[i].1 {
-                break;
-            }
-            j += 1;
+/*
+let mut all1 = 0;
+let mut same1 = 0;
+let mut all2 = 0;
+let mut same2 = 0;
+let mut mall1 = vec![vec![0; 4]; 4];
+let mut mall2 = vec![vec![0; 4]; 4];
+let mut msame1 = vec![vec![0; 4]; 4];
+let mut msame2 = vec![vec![0; 4]; 4];
+let mut total_hcomp1 = 0;
+let mut total_hcomp2 = 0;
+let mut i = 0;
+while i < data.len() {
+    // let j = next_diff12_8(&data, i as i32) as usize;
+    let mut j = i + 1;
+    while j < data.len() {
+        if data[j].0 != data[i].0 || data[j].1 != data[i].1 {
+            break;
         }
-        for k1 in i..j {
-            for k2 in i + 1..j {
-                if data[k1].2 != data[k2].2 {
-                    let lmatch = data[k1].3 == data[k2].3;
-                    let (dref1, dref2) = (data[k1].4, data[k2].4);
-                    // let (cncells1, cncells2) = (data[k1].5, data[k2].5);
-                    // let (isotype1, isotype2) = (&data[k1].6, &data[k2].6);
-                    let (hcomp1, hcomp2) = (data[k1].7, data[k2].7);
-                    if dref1 == 0 || dref2 == 0 {
-                        all1 += 1;
-                        total_hcomp1 += hcomp1 + hcomp2;
-                        if lmatch {
-                            same1 += 1;
-                        }
-                    } else {
-                        all2 += 1;
-                        total_hcomp2 += hcomp1 + hcomp2;
-                        if lmatch {
-                            same2 += 1;
-                        }
+        j += 1;
+    }
+    for k1 in i..j {
+        for k2 in i + 1..j {
+            if data[k1].2 != data[k2].2 {
+                let lmatch = data[k1].3 == data[k2].3;
+                let (dref1, dref2) = (data[k1].4, data[k2].4);
+                // let (cncells1, cncells2) = (data[k1].5, data[k2].5);
+                // let (isotype1, isotype2) = (&data[k1].6, &data[k2].6);
+                let (hcomp1, hcomp2) = (data[k1].7, data[k2].7);
+                if dref1 == 0 || dref2 == 0 {
+                    all1 += 1;
+                    total_hcomp1 += hcomp1 + hcomp2;
+                    if lmatch {
+                        same1 += 1;
                     }
-                    for z1 in 0..4 {
-                        for z2 in z1 + 1..4 {
-                            if data[k1].2 == format!("d{}", z1 + 1)
-                                || data[k2].2 == format!("d{}", z1 + 1)
+                } else {
+                    all2 += 1;
+                    total_hcomp2 += hcomp1 + hcomp2;
+                    if lmatch {
+                        same2 += 1;
+                    }
+                }
+                for z1 in 0..4 {
+                    for z2 in z1 + 1..4 {
+                        if data[k1].2 == format!("d{}", z1 + 1)
+                            || data[k2].2 == format!("d{}", z1 + 1)
+                        {
+                            if data[k1].2 == format!("d{}", z2 + 1)
+                                || data[k2].2 == format!("d{}", z2 + 1)
                             {
-                                if data[k1].2 == format!("d{}", z2 + 1)
-                                    || data[k2].2 == format!("d{}", z2 + 1)
-                                {
-                                    if dref1 == 0 || dref2 == 0 {
-                                        mall1[z1][z2] += 1;
-                                        if lmatch {
-                                            msame1[z1][z2] += 1;
-                                        }
-                                    } else {
-                                        mall2[z1][z2] += 1;
-                                        if lmatch {
-                                            msame2[z1][z2] += 1;
-                                        }
+                                if dref1 == 0 || dref2 == 0 {
+                                    mall1[z1][z2] += 1;
+                                    if lmatch {
+                                        msame1[z1][z2] += 1;
+                                    }
+                                } else {
+                                    mall2[z1][z2] += 1;
+                                    if lmatch {
+                                        msame2[z1][z2] += 1;
                                     }
                                 }
                             }
@@ -235,46 +241,47 @@ fn main() {
                 }
             }
         }
-        i = j;
     }
-    println!(
-        "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
-        100.0 * same1 as f64 / all1 as f64,
-        same1,
-        all1
-    );
-    println!(
-        "for these cells, mean hcomp = {:.1}",
-        total_hcomp1 as f64 / (all1 as f64 * 2.0)
-    );
-    println!(
-        "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
-        100.0 * same2 as f64 / all2 as f64,
-        same2,
-        all2
-    );
-    println!(
-        "for these cells, mean hcomp = {:.1}",
-        total_hcomp2 as f64 / (all2 as f64 * 2.0)
-    );
+    i = j;
+}
+println!(
+    "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
+    100.0 * same1 as f64 / all1 as f64,
+    same1,
+    all1
+);
+println!(
+    "for these cells, mean hcomp = {:.1}",
+    total_hcomp1 as f64 / (all1 as f64 * 2.0)
+);
+println!(
+    "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
+    100.0 * same2 as f64 / all2 as f64,
+    same2,
+    all2
+);
+println!(
+    "for these cells, mean hcomp = {:.1}",
+    total_hcomp2 as f64 / (all2 as f64 * 2.0)
+);
 
-    // The following doesn't make sense for the BIB=@training.
+// The following doesn't make sense for the BIB=@training.
 
-    for z1 in 0..4 {
-        for z2 in z1 + 1..4 {
-            println!("\nd{} versus d{}", z1 + 1, z2 + 1);
-            println!(
-                "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
-                100.0 * msame1[z1][z2] as f64 / mall1[z1][z2] as f64,
-                msame1[z1][z2],
-                mall1[z1][z2],
-            );
-            println!(
-                "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
-                100.0 * msame2[z1][z2] as f64 / mall2[z1][z2] as f64,
-                msame2[z1][z2],
-                mall2[z1][z2],
-            );
-        }
+for z1 in 0..4 {
+    for z2 in z1 + 1..4 {
+        println!("\nd{} versus d{}", z1 + 1, z2 + 1);
+        println!(
+            "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
+            100.0 * msame1[z1][z2] as f64 / mall1[z1][z2] as f64,
+            msame1[z1][z2],
+            mall1[z1][z2],
+        );
+        println!(
+            "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
+            100.0 * msame2[z1][z2] as f64 / mall2[z1][z2] as f64,
+            msame2[z1][z2],
+            mall2[z1][z2],
+        );
     }
-    */
+}
+*/
