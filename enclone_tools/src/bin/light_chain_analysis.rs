e3c37c02807d6cc22531515f37860656889a5dc9
David Jaffe <david.jaffe@10xgenomics.com>
2022-03-15 09:37:11 -0700

    test all pairs of donors

--- enclone_tools/src/bin/light_chain_analysis.rs
+++ enclone_tools/src/bin/light_chain_analysis.rs
@@ -23,6 +23,7 @@ use rayon::prelude::*;
 use std::collections::HashMap;
 use std::env;
 use std::io::BufRead;
+use std::mem::swap;
 use string_utils::TextUtils;
 
 fn main() {
@@ -102,16 +103,73 @@ fn main() {
     // 3. count for equal light chain gene names and dref1 > 0 and dref2 > 0
     // 4. count for unequal light chain gene names and dref1 > 0 and dref2 > 0.
 
-    // Calculate.
+    // Make one pass for all donors, and one pass each for each pair of donors.
+    // This is seven times slower than it should be.
 
-    let mut res = vec![(0, 0, 0, 0); 11];
-    bounds.par_iter_mut().for_each(|res| {
-        let i = res.0;
-        let j = res.1;
-        for k1 in i..j {
-            for k2 in i + 1..j {
-                // require different donors
-                if data[k1].3 != data[k2].3 {
+    for pass in 0..7 {
+        for i in 0..bounds.len() {
+            bounds[i].2 = vec![(0, 0, 0, 0); 11];
+        }
+        if pass == 0 {
+            println!("\nany two donors");
+        } else if pass == 1 {
+            println!("donors 1 and 2");
+        } else if pass == 2 {
+            println!("donors 1 and 3");
+        } else if pass == 3 {
+            println!("donors 1 and 4");
+        } else if pass == 4 {
+            println!("donors 2 and 3");
+        } else if pass == 5 {
+            println!("donors 2 and 4");
+        } else {
+            println!("donors 3 and 4");
+        }
+        let mut res = vec![(0, 0, 0, 0); 11];
+        bounds.par_iter_mut().for_each(|res| {
+            let i = res.0;
+            let j = res.1;
+            for k1 in i..j {
+                for k2 in k1 + 1..j {
+
+                    // Require different donors.
+
+                    let (mut d1, mut d2) = (data[k1].3.clone(), data[k2].3.clone());
+                    if d1 > d2 {
+                        swap(&mut d1, &mut d2);
+                    }
+                    if pass == 0 {
+                        if d1 == d2 {
+                            continue;
+                        }
+                    } else if pass == 1 {
+                        if d1 != "d1" || d2 != "d2" {
+                            continue;
+                        }
+                    } else if pass == 2 {
+                        if d1 != "d1" || d2 != "d3" {
+                            continue;
+                        }
+                    } else if pass == 3 {
+                        if d1 != "d1" || d2 != "d4" {
+                            continue;
+                        }
+                    } else if pass == 4 {
+                        if d1 != "d2" || d2 != "d3" {
+                            continue;
+                        }
+                    } else if pass == 5 {
+                        if d1 != "d2" || d2 != "d4" {
+                            continue;
+                        }
+                    } else {
+                        if d1 != "d3" || d2 != "d4" {
+                            continue;
+                        }
+                    }
+
+                    // Add to results.
+
                     let mut same = 0;
                     for m in 0..data[k1].2.len() {
                         if data[k1].2[m] == data[k2].2[m] {
@@ -138,150 +196,41 @@ fn main() {
                     }
                 }
             }
-        }
-    });
-    for i in 0..bounds.len() {
-        for j in 0..=10 {
-            res[j].0 += bounds[i].2[j].0;
-            res[j].1 += bounds[i].2[j].1;
-            res[j].2 += bounds[i].2[j].2;
-            res[j].3 += bounds[i].2[j].3;
-        }
-    }
-
-    // Print results.
-
-    println!(
-        "\nConsider two cells from different donors that have the same heavy chain gene name\n\
-        and CDR3H length."
-    );
-    println!("\npercent identity on CDR3H-AA ==> probability that light chain genes are the same");
-    println!("\npercent identity rounded down to nearest ten percent");
-    println!("\nboth cells have dref > 0:\n");
-    for j in 0..=10 {
-        let n = res[j].2 + res[j].3;
-        let nznz = 100.0 * res[j].2 as f64 / n as f64;
-        let m = 10 * j;
-        println!("{m}% ==> {nznz:.1}% (n = {n})");
-    }
-    println!("\nboth cells have dref == 0:\n");
-    for j in 0..=10 {
-        let n = res[j].0 + res[j].1;
-        let nznz = 100.0 * res[j].0 as f64 / n as f64;
-        let m = 10 * j;
-        println!("{m}% ==> {nznz:.1}% (n = {n})");
-    }
-    println!("");
-}
-
-/*
-let mut all1 = 0;
-let mut same1 = 0;
-let mut all2 = 0;
-let mut same2 = 0;
-let mut mall1 = vec![vec![0; 4]; 4];
-let mut mall2 = vec![vec![0; 4]; 4];
-let mut msame1 = vec![vec![0; 4]; 4];
-let mut msame2 = vec![vec![0; 4]; 4];
-let mut total_hcomp1 = 0;
-let mut total_hcomp2 = 0;
-let mut i = 0;
-while i < data.len() {
-    // let j = next_diff12_8(&data, i as i32) as usize;
-    let mut j = i + 1;
-    while j < data.len() {
-        if data[j].0 != data[i].0 || data[j].1 != data[i].1 {
-            break;
-        }
-        j += 1;
-    }
-    for k1 in i..j {
-        for k2 in i + 1..j {
-            if data[k1].2 != data[k2].2 {
-                let lmatch = data[k1].3 == data[k2].3;
-                let (dref1, dref2) = (data[k1].4, data[k2].4);
-                // let (cncells1, cncells2) = (data[k1].5, data[k2].5);
-                // let (isotype1, isotype2) = (&data[k1].6, &data[k2].6);
-                let (hcomp1, hcomp2) = (data[k1].7, data[k2].7);
-                if dref1 == 0 || dref2 == 0 {
-                    all1 += 1;
-                    total_hcomp1 += hcomp1 + hcomp2;
-                    if lmatch {
-                        same1 += 1;
-                    }
-                } else {
-                    all2 += 1;
-                    total_hcomp2 += hcomp1 + hcomp2;
-                    if lmatch {
-                        same2 += 1;
-                    }
-                }
-                for z1 in 0..4 {
-                    for z2 in z1 + 1..4 {
-                        if data[k1].2 == format!("d{}", z1 + 1)
-                            || data[k2].2 == format!("d{}", z1 + 1)
-                        {
-                            if data[k1].2 == format!("d{}", z2 + 1)
-                                || data[k2].2 == format!("d{}", z2 + 1)
-                            {
-                                if dref1 == 0 || dref2 == 0 {
-                                    mall1[z1][z2] += 1;
-                                    if lmatch {
-                                        msame1[z1][z2] += 1;
-                                    }
-                                } else {
-                                    mall2[z1][z2] += 1;
-                                    if lmatch {
-                                        msame2[z1][z2] += 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+        });
+        for i in 0..bounds.len() {
+            for j in 0..=10 {
+                res[j].0 += bounds[i].2[j].0;
+                res[j].1 += bounds[i].2[j].1;
+                res[j].2 += bounds[i].2[j].2;
+                res[j].3 += bounds[i].2[j].3;
             }
         }
-    }
-    i = j;
-}
-println!(
-    "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
-    100.0 * same1 as f64 / all1 as f64,
-    same1,
-    all1
-);
-println!(
-    "for these cells, mean hcomp = {:.1}",
-    total_hcomp1 as f64 / (all1 as f64 * 2.0)
-);
-println!(
-    "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
-    100.0 * same2 as f64 / all2 as f64,
-    same2,
-    all2
-);
-println!(
-    "for these cells, mean hcomp = {:.1}",
-    total_hcomp2 as f64 / (all2 as f64 * 2.0)
-);
 
-// The following doesn't make sense for the BIB=@training.
+        // Print results.
 
-for z1 in 0..4 {
-    for z2 in z1 + 1..4 {
-        println!("\nd{} versus d{}", z1 + 1, z2 + 1);
         println!(
-            "interdonor light chain concordance for either naive = {:.1}% = {} of {}",
-            100.0 * msame1[z1][z2] as f64 / mall1[z1][z2] as f64,
-            msame1[z1][z2],
-            mall1[z1][z2],
+            "\nConsider two cells from different donors that have the same heavy chain gene name\n\
+            and CDR3H length."
         );
         println!(
-            "interdonor light chain concordance for neither naive = {:.1}% = {} of {}",
-            100.0 * msame2[z1][z2] as f64 / mall2[z1][z2] as f64,
-            msame2[z1][z2],
-            mall2[z1][z2],
+            "\npercent identity on CDR3H-AA ==> \
+            probability that light chain genes are the same"
         );
+        println!("\npercent identity rounded down to nearest ten percent");
+        println!("\nboth cells have dref > 0:\n");
+        for j in 0..=10 {
+            let n = res[j].2 + res[j].3;
+            let nznz = 100.0 * res[j].2 as f64 / n as f64;
+            let m = 10 * j;
+            println!("{m}% ==> {nznz:.1}% (n = {n})");
+        }
+        println!("\nboth cells have dref == 0:\n");
+        for j in 0..=10 {
+            let n = res[j].0 + res[j].1;
+            let nznz = 100.0 * res[j].0 as f64 / n as f64;
+            let m = 10 * j;
+            println!("{m}% ==> {nznz:.1}% (n = {n})");
+        }
+        println!("");
     }
 }
-*/
