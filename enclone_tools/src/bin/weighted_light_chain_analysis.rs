5e0b4a7967bdc9a48fdda13db6dd8c10728984d9
David Jaffe <david.jaffe@10xgenomics.com>
2022-03-22 04:56:07 -0700

    work in progress

---
+++ enclone_tools/src/bin/weighted_light_chain_analysis.rs
@@ -0,0 +1,182 @@
+// Copyright (c) 2022 10X Genomics, Inc. All rights reserved.
+//
+// See also private_light_chain_analysis.rs.
+//
+// Analyze light chains.  Supply a single file of data, with one line per cell, and fields
+// including donors_cell,v_name1,v_name2,dref,cdr3_aa1,clonotype_ncells,const1.
+//
+// Data from:
+//
+// enclone BCR=@test BUILT_IN CHAINS_EXACT=2 CHAINS=2 NOPRINT POUT=stdout PCELL ECHOC
+//         PCOLS=donors_cell,v_name1,v_name2,dref,cdr3_aa1,clonotype_ncells,const1,hcomp
+//         > per_cell_stuff
+
+use io_utils::*;
+use pretty_trace::PrettyTrace;
+use rayon::prelude::*;
+use std::cmp::max;
+use std::collections::HashMap;
+use std::env;
+use std::io::BufRead;
+use std::mem::swap;
+use string_utils::{stringme, strme, TextUtils};
+use tables::*;
+use vector_utils::unique_sort;
+
+pub fn hcat(col1: &[String], col2: &[String], sep: usize) -> Vec<String> {
+    let mut cat = Vec::<String>::new();
+    let height = max(col1.len(), col2.len());
+    let mut width1 = 0;
+    for x in col1 {
+        width1 = max(width1, x.chars().count() + sep);
+    }
+    for i in 0..height {
+        let mut s = if i < col1.len() {
+            col1[i].clone()
+        } else {
+            String::new()
+        };
+        while s.chars().count() < width1 {
+            s += " ";
+        }
+        if i < col2.len() {
+            s += &col2[i];
+        }
+        cat.push(s);
+    }
+    cat
+}
+
+fn main() {
+    PrettyTrace::new().on();
+    let args: Vec<String> = env::args().collect();
+    let f = open_for_read![&args[1]];
+    let mut first = true;
+    let mut tof = HashMap::<String, usize>::new();
+    let mut data = Vec::<(
+        String,
+        usize,
+        Vec<u8>,
+        String,
+        String,
+        usize,
+        usize,
+        String,
+        usize,
+        usize,
+    )>::new();
+    for line in f.lines() {
+        let s = line.unwrap();
+        if s.starts_with("#") {
+            continue;
+        }
+        let fields = s.split(',').collect::<Vec<&str>>();
+        if first {
+            for i in 0..fields.len() {
+                tof.insert(fields[i].to_string(), i);
+            }
+            first = false;
+        } else {
+            data.push((
+                fields[tof["v_name1"]].to_string(),
+                fields[tof["cdr3_aa1"]].len(),
+                fields[tof["cdr3_aa1"]].to_string().as_bytes().to_vec(),
+                fields[tof["donors_cell"]].to_string(),
+                fields[tof["v_name2"]].to_string(),
+                fields[tof["dref"]].force_usize(),
+                fields[tof["clonotype_ncells"]].force_usize(),
+                fields[tof["const1"]].to_string(),
+                fields[tof["hcomp"]].force_usize(),
+                fields[tof["jun_ins"]].force_usize(),
+            ));
+        }
+    }
+    data.sort();
+
+    // Replace paralogs.
+
+    for i in 0..data.len() {
+        data[i].4 = data[i].4.replace("D", "");
+    }
+
+    // Define groups based on equal heavy chain gene names and CDR3H length.
+    // Plus placeholder for results, see next.
+
+    let mut bounds = Vec::<(usize, usize, Vec<Vec<(usize, usize, usize, usize)>>)>::new();
+    let mut i = 0;
+    while i < data.len() {
+        // let j = next_diff12_9(&data, i as i32) as usize;
+        let mut j = i + 1;
+        while j < data.len() {
+            if data[j].0 != data[i].0 || data[j].1 != data[i].1 {
+                break;
+            }
+            j += 1;
+        }
+        bounds.push((i, j, vec![vec![(0, 0, 0, 0); 11]; 7]));
+        i = j;
+    }
+
+    // Results = for each percent identity, rounded down:
+    // 3. count for equal light chain gene names and dref1 > 0 and dref2 > 0
+    // 4. count for unequal light chain gene names and dref1 > 0 and dref2 > 0.
+    //
+    // Make one pass for all donors, and one pass each for each pair of donors.
+
+    bounds.par_iter_mut().for_each(|res| {
+        let i = res.0;
+        let j = res.1;
+        for k1 in i..j {
+            for k2 in k1 + 1..j {
+                // Require different donors.
+
+                if data[k1].3 == data[k2].3 {
+                    continue;
+                }
+
+                // Compute stuff.
+
+                let mut same = 0;
+                for m in 0..data[k1].2.len() {
+                    if data[k1].2[m] == data[k2].2[m] {
+                        same += 1;
+                    }
+                }
+                let ident = 100.0 * same as f64 / data[k1].2.len() as f64;
+                let ident = ident.floor() as usize;
+                let ident = ident / 10;
+                let (dref1, dref2) = (data[k1].5, data[k2].5);
+                let eq_light = data[k1].4 == data[k2].4;
+
+                if dref1 > 0 && dref2 > 0 {
+                    if eq_light {
+                        res.2[0][ident].2 += 1;
+                    } else {
+                        res.2[0][ident].3 += 1;
+                    }
+                }
+            }
+        }
+    });
+
+    // Sum.
+
+    let mut res = vec![vec![(0, 0, 0, 0); 11]; 7];
+    for i in 0..bounds.len() {
+        for j in 0..=10 {
+            res[0][j].0 += bounds[i].2[0][j].0;
+            res[0][j].1 += bounds[i].2[0][j].1;
+            res[0][j].2 += bounds[i].2[0][j].2;
+            res[0][j].3 += bounds[i].2[0][j].3;
+        }
+    }
+
+    // Print results.
+
+    for j in 0..=10 {
+        let n = res[0][j].2 + res[0][j].3;
+        let nznz = 100.0 * res[0][j].2 as f64 / n as f64;
+        println!("{}% ==> {nznz:.1}% of {n}", 10 * j);
+    }
+}
+        
