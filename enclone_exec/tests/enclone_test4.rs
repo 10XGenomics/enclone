950c8e923f53c5b0535f8142de32dd379929f12e
David Jaffe <david.jaffe@10xgenomics.com>
2022-04-12 12:58:58 -0700

    use github refs for rust-toolbox

--- enclone_exec/tests/enclone_test4.rs
+++ enclone_exec/tests/enclone_test4.rs
@@ -194,7 +194,7 @@ fn test_dupped_crates() {
     unique_sort(&mut crates);
     let n2 = crates.len();
     let d = n1 - n2;
-    const DUPPED_CRATES: usize = 39;
+    const DUPPED_CRATES: usize = 29;
     if d != DUPPED_CRATES {
         eprintln!(
             "\nThe number of duplicated crates is {}, but the required number is {}.\n",
