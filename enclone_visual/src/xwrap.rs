59f49b76b657a46d0bb8c7c0c5d4a898989516ac
David Jaffe <david.jaffe@10xgenomics.com>
2022-04-18 07:44:18 -0700

    tweak

--- enclone_visual/src/xwrap.rs
+++ enclone_visual/src/xwrap.rs
@@ -1,7 +1,5 @@
 // This code is a minimally modified version of code in the crate shotgun:2.2.1.  It is
-// subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0.
+// subject to the terms of the Mozilla Public License, v. 2.0. 
 
 use std::mem;
 use std::os::raw;
