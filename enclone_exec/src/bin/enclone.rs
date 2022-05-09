570fde911939dbde7fae8be7d1abc1981b285f1b
David Jaffe <david.jaffe@10xgenomics.com>
2022-05-09 14:24:10 -0700

    rustfmt

--- enclone_exec/src/bin/enclone.rs
+++ enclone_exec/src/bin/enclone.rs
@@ -163,7 +163,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 evh.svg_hist_uniq.push(svg.clone());
                 evh.svg_history.push(0);
 
-
                 let dataset_names = &res.outs.dataset_names;
                 let metrics0 = &res.outs.metrics;
                 let mut metrics = Vec::<Vec<String>>::new();
@@ -194,7 +193,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     metrics_condensed: false,
                 };
                 let summary = s.pack();
-
 
                 evh.summary_hist_uniq.push(summary);
                 evh.summary_history.push(0);
