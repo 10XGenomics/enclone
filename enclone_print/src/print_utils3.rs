3292fc3d5ce82cf6fe869293e7e4fba424a14363
Nima Mousavi <nima.mousavi@10xgenomics.com>
2022-10-07 15:14:50 -0700

    try break

--- enclone_print/src/print_utils3.rs
+++ enclone_print/src/print_utils3.rs
@@ -615,8 +615,7 @@ pub fn insert_reference_rows(
                 let mut gap = rsi.seq_del_lens[cz] as isize - vlen as isize - jlen as isize;
 
                 if gap < -2 * (trim as isize) {
-                    rows.push(row);
-                    continue;
+                    break;
                     // let mut bcs = Vec::<String>::new();
                     // for u in 0..exacts.len() {
                     //     let ex = &exact_clonotypes[exacts[u]];
